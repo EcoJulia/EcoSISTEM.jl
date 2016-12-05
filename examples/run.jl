5d72e0b71cc3f41ed7fa4c8896472f42691bc4e5
Claire Harris <c.harris.2@research.gla.ac.uk>
2016-12-05 16:23:18 +0000

    run.jl: Plot grid of relative abundance for one species, and overall species richness.

--- examples/run.jl
+++ examples/run.jl
@@ -1,6 +1,7 @@
 using Simulation
 using Diversity
 using Diversity.ShortNames
+using Plots
 # Create a partition
 part=MatrixLandscape(reshape([1, 2, 3, 4, 5, 6, 7, 8], (2, 2, 2)), Habitats([1 2; 3 4]))
 
@@ -15,7 +16,6 @@ b = β(eco)
 sb = subdiv(β(eco), 3)
 m = metadiv(β(eco), 3)
 
-
 # Create a simple habitat matrix
 mat=ones(10, 10)
 # Populate habitat with 10,000 individuals from 50 species
@@ -23,4 +23,9 @@ LS=populate(50, 10000, Habitats(mat))
 # Create ecosystem from habitat, making every species distinct and
 # having the same trait
 eco=Ecosystem(LS,Species(), StringTraits(repmat(["A"],50)))
+# Create grid of species richness
+sr=SR(eco, 10000)
 
+# Plot 
+heatmap(LS.abundances[1, :, :])
+heatmap(sr)
