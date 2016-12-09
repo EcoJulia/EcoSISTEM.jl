1c8b68c6eb155d3e506f29ef11c471d77c58bb9a
Claire Harris <c.harris.2@research.gla.ac.uk>
2016-12-09 14:12:35 +0000

    Simulation.jl: Include new functions to export

--- src/Simulation.jl
+++ src/Simulation.jl
@@ -6,12 +6,12 @@ export Ecosystem, Habitats, StringTraits, MatrixLandscape
 include("Diversity_funcs.jl")
 
 include("Generate.jl")
-export populate, SR
+export populate, SR, create_habitat
 
 include("Dist.jl")
 export jnorm, jexp, jpois, jbinom, junif, jdir
 
 include("Phylo.jl")
-export jtree, jcoal, assign_trait, get_traits
+export jtree, jcoal, assign_traits!, get_traits
 
 end
