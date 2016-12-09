fcb56d3edbfe23353b841cedeb00cfb322b328ca
Claire Harris <c.harris.2@research.gla.ac.uk>
2016-12-09 15:33:36 +0000

    Dist.jl: Add comments

--- src/Dist.jl
+++ src/Dist.jl
@@ -1,21 +1,26 @@
 using Distributions
 
+# Function to sample randomly from the Exponential distribution
 function jexp(theta, n::Int64=1)
   rand(Exponential(theta), n)
 end
+
+# Function to sample randomly from the Poisson distribution
 function jpois(gamma, n::Int64=1)
   rand(Poisson(gamma), n)
 end
 
+# Function to sample randomly from the Binomial distribution
 function jbinom(n::Int64, size::Int64, p::Real)
   rand(Binomial(size,p), n)
 end
 
+# Function to sample randomly from the Multinomial distribution
 function jmulti(n::Int64, p::AbstractArray)
   rand(Multinomial(n, p))
 end
-function jmulti(n::Int64, p::Real)
-  rand(Multinomial(n, repmat([p], n)))
+function jmulti(n::Int64,size::Int64, p::Real)
+  rand(Multinomial(size, repmat([p], n)))
 end
 
 
@@ -29,6 +34,7 @@ function jdir(k,a)
   rand(Dirichlet(k,a))
 end
 
+# Function to sample randomly from the Normal distribution
 function jnorm(μ,σ,n::Int=1)
   rand(Normal(μ,σ),n)
 end
