094f4eca3ddb81f8ecbc15e5296bc64be0724ee1
Claire Harris <c.harris.2@research.gla.ac.uk>
2017-01-12 17:25:45 +0000

    Dist.jl: Write a function for truncated normal distribution

--- src/Dist.jl
+++ src/Dist.jl
@@ -39,10 +39,6 @@ function jnorm(μ,σ,n::Int=1)
   rand(Normal(μ,σ),n)
 end
 
-function norm_x(μ, σ, n::Int = 1)
-  x = jnorm(μ, σ, 1000)
-  min.x = minimum(x)
-  max.x = maximum(x)
-  x.norm = (x - min.x)/(max.x - min.x)
-  rand(x, n)
+function tnorm(μ, σ, l = 0, u = 1, n::Int = 1)
+  rand(Truncated(Normal(μ, σ), l, u), n)
 end
