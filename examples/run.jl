e19d1dd3900791677c560873c97f5f3c2cfb7551
Claire Harris <c.harris.2@research.gla.ac.uk>
2017-03-06 16:12:59 +0000

    run.jl Comment and check remaining code

--- examples/run.jl
+++ examples/run.jl
@@ -320,19 +320,17 @@ sppl = SpeciesList(numSpecies, numTraits, Multinomial(individuals, numSpecies),
                    energy_vec)
 abenv = MatrixAbioticEnv(numNiches, grid, totalK)
 eco = Ecosystem(sppl, abenv, false)
-#print(eco.partition.abundances)
 
-# Run simulations 100 times
-abun = run_sim_grid(eco, param, 1000, false)
+# Run simulation grid
+abun = run_sim_spatial(eco, param, 100, 0, 1, 1, false)
 
+# Plot
 @rput abun
 R" par(mfrow=c(5,5), mar=c(2, 2, 2, 2))
-for (i in 1:5){
-  for (j in 1:5){
+for (i in 1:25){
     for (k in 1:8){
       if (k==1) plot_fun=plot else plot_fun=lines
-        plot_fun(1:101, abun[ ,k, i, j], col=k, xlab='Abundance', ylab='Time', type='l',
+        plot_fun(0:101, abun[ , k, 1, i], col=k, xlab='Abundance', ylab='Time', type='l',
         ylim=c(0, max(abun)))
       }
-    }
   }"
