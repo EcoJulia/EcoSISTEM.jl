11d843609c2900d7c41ecabbcffad91099c1ecdd
Claire Harris <c.harris.2@research.gla.ac.uk>
2017-05-25 17:03:41 +0100

    SpeciesList.jl: Alter type so that it can take in any Abstract Type and not just Phylogeny. Add in a new function to cover this new feature, document and alter diversity API functions so that they work with the new code.

--- src/SpeciesList.jl
+++ src/SpeciesList.jl
@@ -1,4 +1,5 @@
-
+using Diversity.Phylogenetics
+import Diversity.getnames
 """
     SpeciesList{T <: AbstractTraits, R <: AbstractRequirement,
                  MO <: AbstractMovement} <: AbstractTypes
@@ -6,39 +7,49 @@ Species list houses all species-specific information including trait information
 phylogenetic relationships, requirement for energy and movement types.
 """
 type SpeciesList{T <: AbstractTraits,
-                 R <: AbstractRequirement, TR <: AbstractTree,
-                 MO <: AbstractMovement} <: AbstractTypes
-  similarity::Matrix{Float64}
+                 R <: AbstractRequirement,
+                 MO <: AbstractMovement,
+                 P <: AbstractTypes} <: AbstractTypes
   names::Vector{String}
   traits::T
   abun::Vector{Int64}
   requirement::R
-  phylo::TR
+  phylo::P
   movement::MO
 
-  function (::Type{SpeciesList{T, R, TR, MO}}){T <: AbstractTraits,
-                   R <: AbstractRequirement, TR <: AbstractTree,
-                   MO <: AbstractMovement}(similarity::Matrix{Float64},
-      names:: Vector{String}, traits::T, abun::Vector{Int64}, req::R, phylo::TR,
-      movement::MO)
+  function (::Type{SpeciesList{T, R, MO, P}}){T <: AbstractTraits,
+                   R <: AbstractRequirement,
+                   MO <: AbstractMovement,
+                   P <: AbstractTypes}(names:: Vector{String},
+      traits::T, abun::Vector{Int64}, req::R,
+      phylo::P, movement::MO)
       # Assign names
-      names = map(x -> "$x", 1:size(similarity, 1))
+      #names = map(x -> "$x", 1:size(phylo.Zmatrix, 1))
 
       # Check similarity is square matrix
-      size(similarity, 1) == size(similarity, 2) ||
-      throw(DimensionMismatch("Similarity matrix is not square"))
+    #size(phylo.Zmatrix, 1) == size(phylo.Zmatrix, 2) ||
+      #throw(DimensionMismatch("Similarity matrix is not square"))
 
       # Check dimensions of abundance and similarity match
-      length(abun) == size(similarity, 1) ||
-      throw(DimensionMismatch("Similarity matrix does not match abundances"))
+      #length(abun) == size(phylo.Zmatrix, 1) ||
+      #throw(DimensionMismatch("Similarity matrix does not match abundances"))
 
       # Check similarity is bounded between 0 and 1
-      minimum(similarity) ≥ 0 || throw(DomainError())
-      maximum(similarity) ≤ 1 || warn("Similarity matrix has values above 1")
-      new{T, R, TR, MO}(similarity, names, traits, abun, req, phylo, movement)
+      #minimum(phylo.Zmatrix) ≥ 0 || throw(DomainError())
+      #maximum(phylo.Zmatrix) ≤ 1 || warn("Similarity matrix has values above 1")
+      new{T, R, MO, P}(names, traits, abun, req, phylo, movement)
+  end
+  function (::Type{SpeciesList{T, R, MO, P}}){T <: AbstractTraits,
+                   R <: AbstractRequirement,
+                   MO <: AbstractMovement,
+                   P <: AbstractTypes}(
+      traits::T, abun::Vector{Int64}, req::R,
+      phylo::P, movement::MO)
+      # Assign names
+      names = map(x -> "$x", 1:size(phylo.Zmatrix, 1))
+      new{T, R, MO, P}(names, traits, abun, req, phylo, movement)
   end
 end
-
 """
     SpeciesList{R <: AbstractRequirement,
       MO <: AbstractMovement}(numspecies::Int64,
@@ -62,7 +73,7 @@ function SpeciesList{R <: AbstractRequirement,
     # Get traits from tree
     sp_trt = BasicTrait(get_traits(tree, names, true)[:,1])
     # Create similarity matrix (for now identity)
-    similarity = eye(numspecies)
+    phy = Phylogeny(tree)
     # Draw random set of abundances from distribution
     abun = rand(abun_dist)
     # error out when abun dist and NumberSpecies are not the same (same for energy dist)
@@ -70,29 +81,66 @@ function SpeciesList{R <: AbstractRequirement,
                                           doesn't match number species"))
     length(req.energy)==numspecies || throw(DimensionMismatch("Requirement vector
                                           doesn't match number species"))
-    size(similarity)==(numspecies, numspecies) || throw(DimensionMismatch("
-                                Similarity matrix doesn't match number species"))
-
   SpeciesList{typeof(sp_trt), typeof(req),
-              typeof(tree), typeof(movement)}(similarity, names,
-                                              sp_trt, abun, req,
-                                              tree, movement)
+              typeof(movement), typeof(phy)}(names, sp_trt, abun, req,
+                                              phy, movement)
+end
+"""
+    SpeciesList{R <: AbstractRequirement,
+      MO <: AbstractMovement, P <: AbstractTypes}(numspecies::Int64,
+      numtraits::Int64, abun_dist::Distribution, req::R,
+      movement::MO, phy::P)
+Function to create a SpeciesList given a number of species, the number of traits
+they possess, their abundances, requirement from the environment and their
+movement kernel and any type of AbstractTypes. 
+"""
+function SpeciesList{R <: AbstractRequirement,
+    MO <: AbstractMovement, P <: AbstractTypes}(numspecies::Int64,
+    numtraits::Int64, abun_dist::Distribution, req::R,
+    movement::MO, phy::P)
+
+    names = map(x -> "$x", 1:numspecies)
+    # Create tree
+    tree = jcoal(names, 100)
+    # Create traits and assign to tips
+    trts = map(string, 1:numtraits)
+    assign_traits!(tree, 0.5, trts)
+    # Get traits from tree
+    sp_trt = BasicTrait(get_traits(tree, names, true)[:,1])
+    # Draw random set of abundances from distribution
+    abun = rand(abun_dist)
+    # error out when abun dist and NumberSpecies are not the same (same for energy dist)
+    length(abun)==numspecies || throw(DimensionMismatch("Abundance vector
+                                          doesn't match number species"))
+    length(req.energy)==numspecies || throw(DimensionMismatch("Requirement vector
+                                          doesn't match number species"))
+  SpeciesList{typeof(sp_trt), typeof(req),
+              typeof(movement), typeof(phy)}(names, sp_trt, abun, req,
+                                              phy, movement)
+end
+
+function _calcsimilarity(ut::UniqueTypes)
+  return eye(ut.num)
+end
+
+function _calcsimilarity(ph::Phylogeny)
+return ph.Zmatrix
+end
+
+function _getnames(sl::SpeciesList)
+    return getnames(sl.phylo)
 end
 
 function _counttypes(sl::SpeciesList)
-    return size(sl.similarity, 1)
+    return length(getnames(sl))
 end
 
 function _calcsimilarity(sl::SpeciesList)
-    return sl.similarity
+    return _calcsimilarity(sl.phylo)
 end
 
 function _floattypes(::SpeciesList)
     return Set([Float64])
-end
-
-function _getnames(sl::SpeciesList)
-    return sl.names
 end
 
 function _calcordinariness(sl::SpeciesList, a::AbstractArray)
