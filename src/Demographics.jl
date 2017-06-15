aeca42d8509b03a468e9a5c86482a4477f7d196f
Claire Harris <c.harris.2@research.gla.ac.uk>
2017-06-16 00:08:37 +0100

    Demographics.jl: Create parameter type

---
+++ src/Demographics.jl
@@ -0,0 +1,38 @@
+"""
+    AbstractParams
+
+Abstract supertype for all simulation parameter types
+"""
+abstract AbstractParams
+"""
+    PopGrowth <: AbstractParams
+
+Basic parameter type that holds information on a population's birth and death
+rates, `birth` and `death`, as well as how these are altered by energy
+availability
+"""
+type PopGrowth <: AbstractParams
+  birth::Vector{Float64}
+  death::Vector{Float64}
+  l::Float64
+  s::Float64
+
+  function (::Type{PopGrowth})(birth::Vector{Float64}, death::Vector{Float64},
+    l::Float64, s::Float64)
+    l > s || error("l must be greater than s")
+    l >= 0 && s >= 0 || error("l and s must be greater than zero")
+    new(birth, death, l, s)
+  end
+end
+
+type EqualPop <: AbstractParams
+  birth::Float64
+  death::Float64
+  l::Float64
+  s::Float64
+end
+
+function equalpop(params::EqualPop, numspp)
+  PopGrowth(repmat([params.birth], numspp), repmat([params.death], numspp),
+  params.l, params.s)
+end
