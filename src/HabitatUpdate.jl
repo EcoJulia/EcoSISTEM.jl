8ea9920148648304e1f627f179c4b58f8e3012e3
Claire Harris <c.harris.2@research.gla.ac.uk>
2017-09-15 12:55:05 +0100

    HabitatUpdate.jl: Add new update function that removes a habitat at a certain rate

--- src/HabitatUpdate.jl
+++ src/HabitatUpdate.jl
@@ -7,6 +7,14 @@ end
 function NoChange(eco::Ecosystem, timestep::Unitful.Time)
 end
 
+function HabitatLoss(eco::Ecosystem, timestep::Unitful.Time)
+    val = gethabitat(eco).change.rate
+    v = uconvert(unit(timestep)^-1, val)
+    pos = find(eco.abenv.active)
+    smp = sample(pos, jbinom(1, length(pos), ustrip(v))[1])
+    eco.abenv.budget.matrix[smp] = 0.0
+end
+
 function getchangefun(eco::Ecosystem)
   return gethabitat(eco).change.changefun
 end
