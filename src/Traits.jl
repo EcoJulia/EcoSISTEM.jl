a3f9b18942654562d9f0907a281537920052bcfe
Claire Harris <c.harris.2@research.gla.ac.uk>
2017-10-04 12:23:48 +0100

    Traits.jl: Refine code so that there are only two essential types of trait: Discrete and Continuous. These can be collected into TraitCollections and have associated functions to check for continuousness (iscontinuous) and what type each of the levels are (eltype).

--- src/Traits.jl
+++ src/Traits.jl
@@ -1,3 +1,4 @@
+import Base.eltype
 """
     AbstractTraits{T}
 
@@ -10,11 +11,39 @@ abstract type AbstractTraits{T} end
 Basic trait type that holds information on a single trait for each species, of any
 type `T`.
 """
-mutable struct BasicTrait{T} <: AbstractTraits{T}
-  trait::Array{T, 1}
+mutable struct DiscreteTrait{D} <: AbstractTraits{D}
+  val::Array{D, 1}
 end
 
-mutable struct TempTrait{T <: Unitful.Temperature{Float64}} <: AbstractTraits{T}
-  mean::Array{T, 1}
-  var::Array{T, 1}
+
+iscontinuous(trait::DiscreteTrait) = false
+function eltype{D}(trait::DiscreteTrait{D})
+    return D
+end
+mutable struct ContinuousTrait{C <: Number} <: AbstractTraits{C}
+  mean::Array{C, 1}
+  var::Array{C, 1}
+end
+iscontinuous(trait::ContinuousTrait) = true
+function eltype{C}(trait::ContinuousTrait{C})
+    return C
+end
+
+mutable struct TraitCollection2{T1, T2} <: AbstractTraits{Tuple{T1, T2}}
+    t1::T1
+    t2::T2
+end
+iscontinuous(trait::TraitCollection2) = [iscontinuous(trait.t1), iscontinuous(trait.t2)]
+function eltype(trait::TraitCollection2)
+    return [eltype(trait.t1), eltype(trait.t2)]
+end
+mutable struct TraitCollection3{T1, T2, T3} <: AbstractTraits{Tuple{T1, T2, T3}}
+    t1::T1
+    t2::T2
+    t3::T3
+end
+iscontinuous(trait::TraitCollection3) = [iscontinuous(trait.t1),
+iscontinuous(trait.t2), iscontinuous(trait.t3)]
+function eltype(trait::TraitCollection3)
+    return [eltype(trait.t1), eltype(trait.t2), eltype(trait.t3)]
 end
