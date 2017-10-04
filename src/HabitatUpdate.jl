95e14a607bc032f192217a84755568c410171e40
Claire Harris <c.harris.2@research.gla.ac.uk>
2017-10-04 12:55:41 +0100

    HabitatUpdate.jl: Change Habitat Update functions so that they accept a habitat type as well as the entire ecosystem. This is so that the update can be applied sequentially to each habitat layer. Also alter getchangefun() so that the update is now applied sequentially to each habitat layer - change name to habitatupdate!

--- src/HabitatUpdate.jl
+++ src/HabitatUpdate.jl
@@ -1,21 +1,30 @@
-function TempChange(eco::Ecosystem, timestep::Unitful.Time)
-  val = gethabitat(eco).change.rate
+function TempChange(eco::Ecosystem, hab::ContinuousHab, timestep::Unitful.Time)
+  val = hab.change.rate
   v = uconvert(°C/unit(timestep), val)
-  gethabitat(eco).matrix .+= v * timestep
+  hab.matrix .+= v * timestep
 end
 
-function NoChange(eco::Ecosystem, timestep::Unitful.Time)
+function NoChange(eco::Ecosystem, hab::AbstractHabitat, timestep::Unitful.Time)
 end
 
-function HabitatLoss(eco::Ecosystem, timestep::Unitful.Time)
-    val = gethabitat(eco).change.rate
+function HabitatLoss(eco::Ecosystem, hab::AbstractHabitat, timestep::Unitful.Time)
+    val = hab.change.rate
     v = uconvert(unit(timestep)^-1, val)
     pos = find(eco.abenv.active)
     smp = sample(pos, jbinom(1, length(pos), ustrip(v))[1])
     eco.abenv.budget.matrix[smp] = 0.0
-    #eco.abenv.active[smp] = false
 end
 
-function getchangefun(eco::Ecosystem)
-  return gethabitat(eco).change.changefun
+function habitatupdate!(eco::Ecosystem, timestep::Unitful.Time)
+  _habitatupdate!(eco, eco.abenv.habitat, timestep)
+end
+function _habitatupdate!(eco::Ecosystem, hab::Union{DiscreteHab, ContinuousHab}, timestep::Unitful.Time)
+    hab.change.changefun(eco, hab, timestep)
+end
+function _habitatupdate!(eco::Ecosystem, hab::Union{HabitatCollection2, HabitatCollection3}, timestep::Unitful.Time)
+    habnames = fieldnames(hab)
+    results = map(length(habnames)) do h
+        thishab = gethabitat(hab, habnames[h])
+        _habitatupdate!(eco, thishab, timestep::Unitful.Time)
+    end
 end
