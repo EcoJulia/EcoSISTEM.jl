87cdc71d942d3d3307b209c034fad6a3385215cb
Claire Harris <c.harris.2@research.gla.ac.uk>
2017-10-04 12:26:46 +0100

    TraitRelationship.jl: Now TraitRelationships can be combined into a collection, much the same as Traits.jl. There are associated functions (iscontinuous and eltype) so that type checking may occur between SpeciesList and TraitRelationship. There is also a combination function (combineTR) that takes in the type of Relationship, i.e. multiplicative, and this returns a method for combining the outcomes of each TraitRelationship function.

--- src/TraitRelationship.jl
+++ src/TraitRelationship.jl
@@ -1,3 +1,5 @@
+using Unitful
+import Base.eltype
 """
     TraitRelationship
 
@@ -5,8 +7,34 @@ The relationship between a trait and its environment, represented as a Matrix
 of Floats.
 
 """
-mutable struct TraitRelationship
-  traitfun::Function
+abstract type AbstractTraitRelationship{TR}
+end
+
+mutable struct TraitRelationship{TR} <: AbstractTraitRelationship{TR}
+    func::Function
+end
+function eltype{TR}(trait::TraitRelationship{TR})
+    return TR
+end
+
+mutable struct multiplicativeTR2{TR1, TR2} <: AbstractTraitRelationship{Tuple{TR1, TR2}}
+  tr1::TR1
+  tr2::TR2
+end
+function eltype(mtr::multiplicativeTR2)
+    return [eltype(mtr.tr1), eltype(mtr.tr2)]
+end
+mutable struct multiplicativeTR3{TR1, TR2, TR3} <:
+    AbstractTraitRelationship{Tuple{TR1, TR2, TR3}}
+  tr1::TR1
+  tr2::TR2
+  tr3::TR3
+end
+function eltype(mtr::multiplicativeTR3)
+    return [eltype(mtr.tr1), eltype(mtr.tr2), eltype(mtr.tr3)]
+end
+function combineTR(tr::Union{multiplicativeTR2, multiplicativeTR3})
+    return *
 end
 
 function GaussTemp(temp::Unitful.Temperature{Float64}, opttemp::Unitful.Temperature{Float64},
