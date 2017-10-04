d4d9907bad5e2723662b67ee3069fe98cbe0d754
Claire Harris <c.harris.2@research.gla.ac.uk>
2017-10-04 12:59:02 +0100

    Traitfuns.jl: Change setup of traitfun() so that it pulls out the habitat, trait and relationship types and performs differently on them if they are multiple or single. In single situations, trait preferences and current habitat conditions are selected differently depending on whether they are discrete or continuous. In multiple scenarios, this process is applied individually to each layer and the results combined by combineTR().

--- src/Traitfuns.jl
+++ src/Traitfuns.jl
@@ -1,37 +1,72 @@
-
-function TraitFun(eco::Ecosystem, pos::Int64, spp::Int64)
-  TraitFun(getenvtype(eco), eco, pos, spp)
+function traitfun(eco::Ecosystem, pos::Int64, spp::Int64)
+    hab = eco.abenv.habitat
+    trts = eco.spplist.traits
+    rel = eco.relationship
+  _traitfun(hab, trts, rel, pos, spp, iscontinuous(hab))
+end
+function _traitfun(hab::AbstractHabitat, trts::AbstractTraits,
+     rel::AbstractTraitRelationship, pos::Int64,
+     spp::Int64, cont::Array{Bool, 1})
+    traitnames = fieldnames(trts)
+    habnames = fieldnames(hab)
+    relnames = fieldnames(rel)
+    results = map(length(traitnames)) do tr
+        thishab = gethabitat(hab, habnames[tr])
+        thistrt = getpref(trts, traitnames[tr])
+        thisrel = getrelationship(rel, relnames[tr])
+        _traitfun(thishab, thistrt, thisrel, pos, spp, iscontinuous(thishab))
+    end
+    combineTR(rel)(results)
+end
+function _traitfun(hab::ContinuousHab, trts::ContinuousTrait,
+    rel::AbstractTraitRelationship, pos::Int64, spp::Int64, cont::Bool)
+        h = gethabitat(hab, pos)
+        mean, var = getpref(trts, spp)
+    return getrelfun(rel)(h, mean, var)
 end
 
-function TraitFun(env::Type{ContinuousHab{Temp}}, eco::Ecosystem, pos::Int64, spp::Int64)
-  T = gethabitat(eco, pos)
-  T_opt, Var = getpref(env, eco, spp)
-  return gettraitfun(eco)(T, T_opt, Var)
+function _traitfun(hab::DiscreteHab, trts::DiscreteTrait,
+    rel::AbstractTraitRelationship, pos::Int64, spp::Int64, cont::Bool)
+        currentniche = gethabitat(hab, pos)
+        preference = getpref(trts, spp)
+    return getrelfun(rel)(currentniche, preference)
 end
 
-function TraitFun(env::Type{DiscreteHab{Niches}}, eco::Ecosystem, pos::Int64, spp::Int64)
-  currentniche = gethabitat(eco, pos)
-  preference = getpref(env, eco, spp)
-  return gettraitfun(eco)(currentniche, preference)
-end
+#function TraitFun(eco::Ecosystem, pos::Int64, spp::Int64, cont::Bool = true)
+#  T = gethabitat(eco, pos)
+#  T_opt, Var = getpref(env, eco, spp)
+#  return gettraitfun(eco)(T, T_opt, Var)
+#end
 
-function TraitFun(env::Type{ContinuousHab{None}}, eco::Ecosystem, pos::Int64, spp::Int64)
-  return 1.0
-end
+#function TraitFun(eco::Ecosystem, pos::Int64, spp::Int64, cont::Bool = false)
+#  T = gethabitat(eco, pos)
+#  T_opt, Var = getpref(env, eco, spp)
+#  return gettraitfun(eco)(T, T_opt, Var)
+#end
+#function TraitFun(env::Type{DiscreteHab{Niches}}, eco::Ecosystem, pos::Int64, spp::Int64)
+#  currentniche = gethabitat(eco, pos)
+#  preference = getpref(env, eco, spp)
+#  return gettraitfun(eco)(currentniche, preference)
+#end
 
-function gettraitfun(eco::Ecosystem)
-  return eco.relationship.traitfun
-end
-
-function getpref{E <: Envtype}(env::E, eco::Ecosystem, spp::Int64)
-  getpref(env, eco, spp)
-end
-
-function getpref(env::Type{ContinuousHab{Temp}}, eco::Ecosystem, spp::Int64)
-  traits = eco.spplist.traits
+#function TraitFun(env::Type{ContinuousHab{None}}, eco::Ecosystem, pos::Int64, spp::Int64)
+#  return 1.0
+#end
+function getpref(traits::ContinuousTrait, spp::Int64)
   return traits.mean[spp], traits.var[spp]
 end
 
-function getpref(env::Type{DiscreteHab{Niches}}, eco::Ecosystem, spp::Int64)
-  return eco.spplist.traits.trait[spp]
+function getpref(traits::DiscreteTrait, spp::Int64)
+  return traits.val[spp]
+end
+
+function getpref(traits::AbstractTraits, field::Symbol)
+  return getfield(traits, field)
+end
+
+function getrelfun(rel::AbstractTraitRelationship)
+  return rel.func
+end
+function getrelationship(rel::AbstractTraitRelationship, field::Symbol)
+  return getfield(rel, field)
 end
