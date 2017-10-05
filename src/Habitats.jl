f1f41bedeca0f17ca14fa2016f7c26f707756183
Claire Harris <c.harris.2@research.gla.ac.uk>
2017-10-05 11:51:38 +0100

    Habitats.jl: Remove unnecessary code for degraded habitats, this has moved to Scenarios.jl.

--- src/Habitats.jl
+++ src/Habitats.jl
@@ -242,6 +242,7 @@ function simplehabitat(val::Unitful.Quantity, size::Unitful.Length,
 
   ContinuousHab(M, size, HabitatUpdate{Unitful.Dimension{()}}(NoChange, 0.0/s))
 end
+
 function simplehabitat(val::Float64, size::Unitful.Length,
   dim::Tuple{Int64, Int64})
   M = Array{Float64}(dim)
@@ -249,13 +250,7 @@ function simplehabitat(val::Float64, size::Unitful.Length,
 
   ContinuousHab(M, size, HabitatUpdate{Unitful.Dimension{()}}(NoChange, 0.0/s))
 end
-function degradedhab(val::Union{Float64, Unitful.Quantity}, size::Unitful.Length,
-  dim::Tuple{Int64, Int64}, rate::Quantity{Float64, typeof(𝐓^-1)})
-  M = Array{Float64}(dim)
-  fill!(M, val)
 
-  ContinuousHab(M, size, HabitatUpdate{Unitful.Dimension{()}}(HabitatLoss, rate))
-end
 function tempgrad(min::Unitful.Temperature{Float64}, max::Unitful.Temperature{Float64},
   size::Unitful.Length{Float64},
   dim::Tuple{Int64, Int64}, rate::Quantity{Float64, typeof(𝚯*𝐓^-1)})
