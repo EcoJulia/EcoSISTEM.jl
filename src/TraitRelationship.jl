8fc7046bac0747040ad6ec378e6fe6fb8ae2c228
Claire Harris <c.harris.2@research.gla.ac.uk>
2017-10-10 13:07:15 +0100

    TraitRelationship: GaussTemp() seemed to specific, I imagine that a Gaussian relationship may come up reasonably often so have made the function more generic - able to accept a range of Unitful quantities as well as floats.

--- src/TraitRelationship.jl
+++ src/TraitRelationship.jl
@@ -13,6 +13,7 @@ end
 mutable struct TraitRelationship{TR} <: AbstractTraitRelationship{TR}
     func::Function
 end
+
 function eltype{TR}(trait::TraitRelationship{TR})
     return TR
 end
@@ -30,19 +31,25 @@ mutable struct multiplicativeTR3{TR1, TR2, TR3} <:
   tr2::TR2
   tr3::TR3
 end
+
 function eltype(mtr::multiplicativeTR3)
     return [eltype(mtr.tr1), eltype(mtr.tr2), eltype(mtr.tr3)]
 end
+
 function combineTR(tr::Union{multiplicativeTR2, multiplicativeTR3})
     return *
 end
 
-function GaussTemp(temp::Unitful.Temperature{Float64}, opttemp::Unitful.Temperature{Float64},
-  var::Unitful.Temperature{Float64})
-  1°C/sqrt(2 * π * var^2) * exp(-abs(temp - opttemp)^2/(2 * var^2))
+function GaussFunc(current::Float64, opt::Float64, var::Float64)
+    1.0 /sqrt(2 * π * var^2) * exp(-abs(current - opt)^2/(2 * var^2))
 end
 
-function SimpleNiche(niche::Int64, pref::Int64)
+function GaussFunc{T <: Unitful.Quantity{Float64}}(current::T,
+      opt::T, var::T)
+    (1.0 * unit(current))/sqrt(2 * π * var^2) * exp(-abs(current - opt)^2/(2 * var^2))
+end
+
+function SimpleDiscrete(niche::Int64, pref::Int64)
   if niche == pref
     return 1.0
   else
@@ -52,6 +59,7 @@ end
 function NoRel(niche::Int64, pref::Int64)
     return 1.0
 end
-function NoRel(val::Float64, optval::Float64, var::Float64)
+function NoRel{T <: Union{Float64, Unitful.Quantity{Float64}}}(val::T,
+     optval::T, var::T)
     return 1.0
 end
