e44290b49eec68af489b3c6cc52d5497968c1dd2
Claire Harris <c.harris.2@research.gla.ac.uk>
2017-11-07 14:49:13 +0000

    AdditionalDiversity.jl: Add simpson, shannon and species richness methods, written to be used like the diversity framework

---
+++ src/AdditionalDiversity.jl
@@ -0,0 +1,40 @@
+using Diversity
+"""
+    meta_simpson(eco::Ecosystem, qs::Vector{Float64})
+Function to calculate the Simpson diversity for the entire ecosystem.
+"""
+function meta_simpson(eco::Ecosystem, qs::Vector{Float64})
+    div = meta_gamma(eco, 2.0)
+    div[:diversity] = 1 ./ div[:diversity]
+    return div
+end
+
+function meta_simpson(eco::Ecosystem, qs::Float64)
+    div = meta_gamma(eco, 2.0)
+    div[:diversity] = 1 ./ div[:diversity]
+    return div
+end
+"""
+    meta_shannon(eco::Ecosystem, qs::Vector{Float64})
+Function to calculate the Shannon entropy for the entire ecosystem.
+"""
+function meta_shannon(eco::Ecosystem, qs::Vector{Float64})
+    div = meta_gamma(eco, 1.0)
+    div[:diversity] = log(div[:diversity])
+    return div
+end
+function meta_shannon(eco::Ecosystem, qs::Float64)
+    div = meta_gamma(eco, 1.0)
+    div[:diversity] = log(div[:diversity])
+    return div
+end
+"""
+    meta_speciesrichness(eco::Ecosystem, qs::Vector{Float64})
+Function to calculate the species richness for the entire ecosystem.
+"""
+function meta_speciesrichness(eco::Ecosystem, qs::Vector{Float64})
+    return meta_gamma(eco, 0.0)
+end
+function meta_speciesrichness(eco::Ecosystem, qs::Float64)
+    return meta_gamma(eco, 0.0)
+end
