705e735ae1fb9277594787dc821a5f0df97f2f1d
Claire Harris <c.harris.2@research.gla.ac.uk>
2017-11-07 14:37:39 +0000

    Demographics.jl: Update documentation of functions and types.

--- src/Demographics.jl
+++ src/Demographics.jl
@@ -9,7 +9,9 @@ abstract type AbstractParams end
 
 Basic parameter type that holds information on a population's birth and death
 rates, `birth` and `death`, as well as how these are altered by energy
-availability
+availability. `l` represents the longevity of species based on their energy
+requirements and `s` is the survival of species dependent on how well their
+traits reflect the environment.
 """
 mutable struct PopGrowth{U <: Rates} <: AbstractParams
   birth::Vector{Quantity{Float64, typeof(𝐓^-1), U}}
@@ -26,7 +28,17 @@ mutable struct PopGrowth{U <: Rates} <: AbstractParams
     new{U}(birth, death, l, s, boost)
   end
 end
+"""
+    EqualPop <: AbstractParams
 
+Parameter type that holds information on a population's birth and death
+rates, `birth` and `death`, specifically populations where all species have the
+same information. `l` represents the longevity of species based on their energy
+requirements and `s` is the survival of species dependent on how well their
+traits reflect the environment. Finally `boost` is used to manipulate how much
+of a boost the species get from being in an environment with lots of available
+energy.
+"""
 mutable struct EqualPop <: AbstractParams
   birth::Quantity{Float64, typeof(𝐓^-1)}
   death::Quantity{Float64, typeof(𝐓^-1)}
@@ -34,7 +46,12 @@ mutable struct EqualPop <: AbstractParams
   s::Float64
   boost::Float64
 end
+"""
+    equalpop(params::EqualPop, numspp)
 
+Function that takes demographic parameters from type `EqualPop` and converts
+them into type `PopGrowth` based on the number of species (`numspp`).
+"""
 function equalpop(params::EqualPop, numspp)
   u = unit(params.birth)
   PopGrowth{typeof(u)}(repmat([params.birth], numspp), repmat([params.death], numspp),
