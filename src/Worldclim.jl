e6fbb7d24951c0d07b586d2b28fe57ea40be3a91
Claire Harris <c.harris.2@research.gla.ac.uk>
2017-12-05 15:56:16 +0000

    Worldclim.jl: Code for loading worldclim csvs and tiff files - Not yet complete

--- src/Worldclim.jl
+++ src/Worldclim.jl
@@ -1,8 +1,54 @@
-using FileIO
-using Images
-using ImageView
-using RasterIO
+using DataStructures
+#using ArrayViews
+using DataFrames
+using JLD
+using RCall
+using AxisArrays
+addprocs(6)
+using JuliaDB
+dat = loadtable("/Users/claireh/Documents/PhD/Data/Worldclim/CSV",
+usecache=false)
 
-img = RasterIO.openraster(Pkg.dir("Simulation", "examples/data/Worldclim",
- "wc2.0_bio_30s_01.tif"))
-imshow(img)
+dat = readtable(
+  "/Users/claireh/Documents/PhD/Data/Worldclim/CSV/AnnualMeanTemp.csv",
+  header = false)
+datarray = DataArray{Float64}(dat)[end:-1:1, :]
+avMeanTemp = AxisArray(datarray[2:nrow(dat), 1:(ncol(dat)-1)],
+    Axis{:x}(datarray[1:999, 1]),
+    Axis{:y}(datarray[end,2:43201]))
+
+using Dagger
+
+load(Dagger.chunk,
+"/Users/claireh/Documents/PhD/Data/Worldclim/CSV/AnnualMeanTemp.csv")
+
+using Images
+using RCall
+@rlibrary("raster")
+R"
+img = raster('~/Documents/PhD/Data/Worldclim/wc2.0_30s_tmin/wc2.0_30s_tmin_01.tif')
+"
+@rget img
+
+img =
+Images.load("/Users/claireh/Documents/PhD/Data/Worldclim/wc2.0_30s_tmin/wc2.0_30s_tmin_01.tif")
+
+function closest_index(x::DataArray{Float64, 1}, val::Float64)
+    return Int(round((val- x[1]) / stepsize))
+end
+
+
+function findval(x::Float64, y::Float64, array::AxisArray{Float64, 2})
+    x < maximum(axes(array)[1].val) && x > minimum(axes(array)[1].val) ||
+    error("X coordinate is out of bounds")
+    y < maximum(axes(array)[2].val) && y > minimum(axes(array)[2].val) ||
+    error("Y coordinate is out of bounds")
+    xval = closest_index(axes(array)[1].val, x)
+    yval = closest_index(axes(array)[2].val, y)
+    return array[xval, yval]
+end
+
+function extractvalues(x::Array{Float64, 1}, y::Array{Float64, 1},
+     array::AxisArray{Float64, 2})
+     return map((i, j) -> findval(i, j, array), x, y)
+end
