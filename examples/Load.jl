f9e895ea78e5be557215eebb57912a81f748295f
Claire Harris <c.harris.2@research.gla.ac.uk>
2018-01-23 23:33:10 +0000

    Load.jl: Code for loading GBIF  data as JuliaDB and saving as Dagger

--- examples/Load.jl
+++ examples/Load.jl
@@ -1,6 +1,31 @@
-include("../src/Dist.jl")
-include("../src/Phylo.jl")
-include("../src/Ecosystem.jl")
-include("../src/Generate.jl")
-include("../src/Helper.jl")
-include("../src/plotting.jl")
+addprocs(8)
+@everywhere using JuliaDB
+latlon = loadndsparse("final",
+       indexcols = [:phylum, :class, :order, :family, :genus, :species, :scientificname],
+       type_detect_rows =5000,
+       colparsers=Dict(:datasetkey=>String,
+                       :occurrenceid=>String,
+                       :gbifid=>String,
+                       :locality=>String,
+                       :publishingorgkey=>String,
+                       :taxonkey=>String,
+                       :institutioncode=>String,
+                       :catalognumber=>String,
+                       :recordnumber=>String))
+save(latlon, "output")
+
+addprocs(4)
+@everywhere using JuliaDB
+latlon = loadndsparse("test",
+       indexcols = [:phylum, :class, :order, :family, :genus, :species, :scientificname],
+       type_detect_rows =5000,
+       colparsers=Dict(:datasetkey=>String,
+                       :occurrenceid=>String,
+                       :gbifid=>String,
+                       :locality=>String,
+                       :publishingorgkey=>String,
+                       :taxonkey=>String,
+                       :institutioncode=>String,
+                       :catalognumber=>String,
+                       :recordnumber=>String))
+save(latlon, "output")
