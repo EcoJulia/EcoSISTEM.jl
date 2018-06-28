76a607a8741ce7e28064a9313d0692008a759f62
Claire Harris <c.harris.2@research.gla.ac.uk>
2018-06-28 16:03:26 +0100

    Simulation.jl: Update export functions

--- src/Simulation.jl
+++ src/Simulation.jl
@@ -1,30 +1,32 @@
 module Simulation
 
 include("Dist.jl")
-export jnorm, jexp, jpois, jbinom, junif, jdir, jmulti
+export jnorm, jexp, jpois, jbinom, junif, jdir, jmulti, Trapezoid
 
 include("Phylo.jl")
 export jtree, jcoal, assign_traits!, get_traits
 
 include("TraitRelationship.jl")
 export TraitRelationship,multiplicativeTR2, multiplicativeTR3, Gauss,
-Match, NoRelContinuous, NoRelDiscrete
+ Match, NoRelContinuous, NoRelDiscrete, Trapeze, Unif,
+ additiveTR2, additiveTR3
 
 include("Habitats.jl")
-export ContinuousHab, DiscreteHab, HabitatCollection2, HabitatCollection3,
+export ContinuousHab,ContinuousTimeHab, DiscreteHab, HabitatCollection2, HabitatCollection3,
  tempgrad
 
 include("Energy.jl")
-export SimpleRequirement
+export SimpleRequirement, SolarRequirement, SimpleBudget, SolarBudget
 
 include("AbioticEnv.jl")
-export GridAbioticEnv, simplenicheAE, tempgradAE, simplehabitatAE, degradedhabitatAE
+export GridAbioticEnv, simplenicheAE, tempgradAE, simplehabitatAE, degradedhabitatAE,
+    eraAE, worldclimAE
 
 include("Movement.jl")
 export GaussianKernel, BirthOnlyMovement, AlwaysMovement, NoMovement, getkernel
 
 include("Traits.jl")
-export ContinuousTrait, DiscreteTrait, TraitCollection2, TraitCollection3,
+export ContinuousTrait, DiscreteTrait, TempBin,RainBin, TraitCollection2, TraitCollection3,
 DiscreteEvolve, ContinuousEvolve
 
 include("Demographics.jl")
@@ -36,32 +38,32 @@ export SpeciesList
 include("Landscape.jl")
 export GridLandscape
 
-#include("Ecosystem.jl")
-#export Ecosystem, getsize, gethabitat, gettraitrel, getgridsize,
-# getdispersaldist, getdispersalvar, resetrate!
+include("Ecosystem.jl")
+export Ecosystem, getsize, gethabitat, gettraitrel, getgridsize,
+ getdispersaldist, getdispersalvar, resetrate!, getbudget
 
-#include("Traitfuns.jl")
-#export TraitFun, getpref, gettraitrel, gethabitat
+include("Traitfuns.jl")
+export TraitFun, getpref, gettraitrel, gethabitat
 
-#include("HabitatUpdate.jl")
-#export getchangefun, TempChange
+include("HabitatUpdate.jl")
+export getchangefun, TempChange, eraChange, worldclimChange
 
-#include("Scenarios.jl")
-#export SimpleScenario, RandHabitatLoss!, ClustHabitatLoss!, DisturbanceScenario,
-#HabitatDisturbance!, UniformDecline, ProportionalDecline, LargeDecline, RareDecline,
-#CommonDecline, HabitatReplacement, Invasive
+include("Scenarios.jl")
+export SimpleScenario, RandHabitatLoss!, ClustHabitatLoss!, DisturbanceScenario,
+ HabitatDisturbance!, UniformDecline, ProportionalDecline, LargeDecline, RareDecline,
+ CommonDecline, HabitatReplacement, Invasive
 
-#include("Generate.jl")
-#export populate!, repopulate!,reenergise!, randomniches, update!, update_birth_move!,
- #convert_coords, get_neighbours
+include("Generate.jl")
+export populate!, repopulate!,reenergise!, randomniches, update!, update_birth_move!,
+ convert_coords, get_neighbours
 
-#include("Helper.jl")
-#export simulate!, simulate_record!,simulate_record_diversity!, expected_counts, generate_storage
+include("Helper.jl")
+export simulate!, simulate_record!,simulate_record_diversity!, expected_counts, generate_storage
 
-#include("plotting.jl")
-#export plot_move, plot_abun,plot_mean,plot_diversity, plot_divergence, freq_hist, plotdiv
+include("plotting.jl")
+export plot_move, plot_abun,plot_mean,plot_diversity, plot_divergence, freq_hist, plotdiv
 
-#include("AdditionalDiversity.jl")
-#export meta_simpson, meta_shannon, meta_speciesrichness
+include("AdditionalDiversity.jl")
+export meta_simpson, meta_shannon, meta_speciesrichness
 
 end
