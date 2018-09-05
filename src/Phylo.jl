db9db9ce8b3392d0dada25f63bf85d8b74927080
Claire Harris <c.harris.2@research.gla.ac.uk>
2018-09-05 15:33:51 +0100

    Phylo.jl: Remove no longer necessary functions - Phylo code can produce these trees.

--- src/Phylo.jl
+++ src/Phylo.jl
@@ -3,63 +3,6 @@ using Distributions
 using DataFrames
 import Phylo.NodeNameIterator
 
-"""
-    jtree(SpeciesNames::Vector{String}, T::Type = String,
-    dist::Distribution = Uniform(0, length(SpeciesNames)))
-
-Function to create a random non-ultrametric phylogenetic tree, with branch
-lengths drawn from a distribution, `dist`, and node information of type, `T`.
-"""
-function jtree(SpeciesNames::Vector{String}, T::Type = Int64,
-  dist::Distribution = Uniform(0, length(SpeciesNames)))
-  #Create tree
-  tree1 = BinaryTree{LeafInfo, Vector{T}}(SpeciesNames)
-  n = length(SpeciesNames)
-  # If only one branch
-  leaf_names = getleafnames(tree1)
-  inner_names = addnodes!(tree1, n-1)
-  if n == 1
-    error("More species needed")
-  elseif n == 2
-    map(x -> addbranch!(tree1, inner_names[1], x, rand(dist)), leaf_names)
-  else
-    inner_names_copy = copy(inner_names)
-    choose_node = inner_names[1]
-    avail_nodes = inner_names
-    # Set up internal structure
-    # Connect all internal nodes together
-    while !isempty(avail_nodes)
-      deleteat!(inner_names, find(inner_names .== choose_node))
-
-      if length(avail_nodes) > 1
-        howmany = rand(1:2)
-      else
-        howmany = 1
-      end
-
-      targets = sample(avail_nodes, howmany, replace = false)
-      map(x -> addbranch!(tree1, choose_node, x, rand(dist)), targets)
-
-      avail_nodes = inner_names[map(x -> indegree(tree1, x) < 1, inner_names)]
-      choose_node = rand(targets, 1)[1]
-    end
-
-    # Add on leaf nodes
-    count = 0
-    while !isempty(leaf_names) && count <= length(inner_names_copy)
-      count = count + 1
-      if outdegree(tree1, inner_names_copy[count]) < 2
-        targets = sample(leaf_names, 2 - outdegree(tree1, inner_names_copy[count]),
-                            replace = false)
-        map(x -> addbranch!(tree1, inner_names_copy[count], x, rand(dist)), targets)
-        deleteat!(leaf_names, findin(leaf_names, targets))
-      end
-    end
-  end
-  tree1
-end
-#tree = jtree(5)
-#plot(tree)
 
 # Function to produce a matrix of all source and target nodes within a tree
 function sou_tar(tree::AbstractTree, len::Bool)
@@ -112,56 +55,7 @@ function root_to_tips(tree)
   end
   paths
 end
-"""
-    jcoal(SpeciesNames::Vector{String}, len::Float64, T::Type = String)
 
-Function to create a random ultrametric phylogenetic tree, with branches
-of length `len`, and node information of type, `T`.
-"""
-function jcoal(SpeciesNames::Vector{String}, len::Float64, T::Type=Int64)
-  # Create random tree
-  tree2=jtree(SpeciesNames, T)
-  n = length(SpeciesNames)
-  if n == 1
-    tree2.branches[1].length = len
-  else
-  # Find tips of tree
-  tips=NodeIterator(tree2, isleaf)
-  # Find paths of each tip from root
-  paths=root_to_tips(tree2)
-  # Create array of source and target nodes
-  paths_mat=sou_tar(tree2, false)
-
-  # Loop through all the paths and calculate branch lengths
-  while length(paths)>0
-    # How many branches does each path have?
-    howmany=map(length, paths)
-    # Select maximum
-    maxpath=findmax(howmany)
-    choosepath=paths[maxpath[2]]
-    # Split path into pairs of nodes
-    pairs=pair(choosepath)
-    # Find the row each pair of nodes corresponds to
-    rows=find_rows(paths_mat, pairs[:,1], pairs[:,2])
-    # Calculate how long the path is already
-    length_left=sum(paths_mat[rows, 2])
-    # Remove rows that have already been assigned a length
-    rows=rows[paths_mat[rows, 2] .== 0]
-    # Calculate branch lengths from Dirichlet distribution
-    pathlength=jdir(length(rows),1)*(len-length_left)
-    # Assign branch lengths to matrix
-    paths_mat[rows,2]=pathlength
-    # Remove completed path
-    deleteat!(paths, maxpath[2])
-  end
-  # Loop through and assign new branch lengths to tree
-  for j in collect(1:size(paths_mat,1))
-      tree2.branches[j].length=paths_mat[j, 2]
-  end
-end
-  # Return ultrametric tree
-  tree2
-end
 
 
 
