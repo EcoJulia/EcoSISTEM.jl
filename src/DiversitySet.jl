19b7b3e5986317da946f07eead60684091a9bc39
Claire Harris <c.harris.2@research.gla.ac.uk>
2018-10-11 14:58:30 +0100

    DiversitySet.jl: Fix bug so that file is only read when non-empty

--- src/DiversitySet.jl
+++ src/DiversitySet.jl
@@ -17,12 +17,12 @@ function updatesimulation!(cache::CachedEcosystem, tm::Unitful.Time)
 end
 
 function gettimes(div::DiversitySet)
-    file = searchdir(div.folder, ".feather")[1]
+    file = searchdir(div.folder, ".feather")
     if ismissing(div.data) & isempty(file)
         return div.times
     else
         if ismissing(div.data) & !isempty(file)
-            div.data = Feather.read(joinpath(div.folder, file))
+            div.data = Feather.read(joinpath(div.folder, file[1]))
             div.data[:type_name] = ""
             div.data[:time] *= 1s
         end
