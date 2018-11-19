aa7b4c755d0fd8f42643573ea3183be3f04573a1
Claire Harris <c.harris.2@research.gla.ac.uk>
2018-11-19 10:50:53 +0000

    Santini_static_traditional.jl: Remove unnecessary run code and alter plotting.

--- examples/Santini_static_traditional.jl
+++ examples/Santini_static_traditional.jl
@@ -47,8 +47,6 @@ grid = (50, 50)
 area = 10000.0km^2
 totalK = 1000000.0 * (numSpecies + numInvasive)
 individuals=20000 * numSpecies
-probs = rand(LogNormal(1.0, 0.5), numSpecies)
-probs /= sum(probs)
 
 # Create movement type - all individuals are allowed to move and have a wide range
 kernel = GaussianKernel(0.0km, numSpecies+numInvasive, 10e-04)
@@ -147,41 +145,7 @@ function linmod(x)
 end
 slopemat = mapslices(linmod, stanmat[1, :, :, :, :], 2)[:, 1, :, :]
 meanslope = mapslices(mean, slopemat, 3)[:, :, 1] * 10
-repslope = mapslices(x-> (sum(x .> 0)/length(x)) > 0.95 || (sum(x .< 0)/length(x)) > 0.95, slopemat, 3)[:, :, 1]
-repslope = reshape(transpose(repslope), 70, 1)
-ms = reshape(transpose(meanslope), 70, 1)
-rep = Vector{String}(70)
-for i in 1:70
-    if repslope[i]
-        rep[i] = ifelse(ms[i] > 0 , "+", "-")
-    else
-        rep[i] = ""
-    end
-end
-@rput meanslope
-@rput repslope
-@rput rep
-R"
-library(fields);
-library(viridis);library(RColorBrewer)
-png('Meanslope_static150_trad.png', width = 1000, height = 1000)
-par(mfrow=c(1,1), mar=c(6,4,4,10));
-image(meanslope, axes=FALSE, xlab='', ylab='', srt=45, col = colorRampPalette(brewer.pal(11, 'RdBu'))(51),
-   breaks =seq(-0.3, 0.3,length.out=52));
-axis(1, at = seq(0,1, length.out=7), cex.axis=1.2,
-labels = c('Sorenson', 'Richness', 'Mean abun',
-'Geometric mean','Simpson', 'Shannon',  'PD'));
-axis(2, at = seq(0,1, length.out=10), cex.axis=1.2,
-labels = c('Uniform', 'Proportional', 'Largest', 'Rarest', 'Common','Invasive',
-'Phylo \n invasive', 'Rand hab \n Loss', 'Clust hab \n Loss', 'Susceptible'));
-image.plot(meanslope, col = colorRampPalette(brewer.pal(11, 'RdBu'))(51), legend.only=TRUE,
-   breaks =seq(-0.3, 0.3,length.out=52),legend.args=list( text='Slope',
-   cex=1.2, side=4,padj = 0.8, line=2), legend.mar = 7)
-mat = expand.grid(seq(0,1, length.out=10), seq(0,1, length.out=7));
-mat = mat[repslope, ]
-points(mat[,2], mat[,1], pch=rep, col ='grey20', cex = 1.5, font = 2)
-dev.off()
-"
+
 # Plot trends over time
 @rput stanmat
 standat = DataFrame(div = reshape(stanmat[1,:,:,1,:], 847000))
@@ -284,122 +248,150 @@ early = stanmat[:,:, 1:24,:,:]
 
 slopematE = mapslices(linmod, early[1, :, :, :, :], 2)[:, 1, :, :]
 meanslopeE = mapslices(mean, slopematE, 3)[:, :, 1]
-repslopeE = mapslices(x-> (sum(x .> 0)/length(x)) > 0.95 || (sum(x .< 0)/length(x)) > 0.95, slopematE, 3)[:, :, 1]
-repslopeE = reshape(repslopeE, 63, 1)
-
+sdmatE = mapslices(std, slopematE, 3)[:, :, 1]
+upperE = meanslopeE .+ sdmatE
+lowerE = meanslopeE .- sdmatE
+repE = Array{String, 2}(7, 10)
+for i in 1:7
+    for j in 1:10
+    if sum(slopematE[i,j,:] .> 0)/1000 >= 0.95 || sum(slopematE[i,j,:] .< 0)/1000 >= 0.95
+        repE[i, j ] = "*"
+    else
+        repE[i, j] = ""
+    end
+end
+end
+@rput repE
 @rput meanslopeE
-@rput repslopeE
-R"
-library(fields);
-library(viridis);library(RColorBrewer)
-png('Meanslope_static150_trad.png', width = 1000, height = 900)
-par(mfrow=c(1,1), mar=c(6,4,4,10));
-image(meanslope, axes=FALSE, xlab='', ylab='', srt=45, col = colorRampPalette(brewer.pal(11, 'RdBu'))(51),
-   breaks =seq(-0.5, 0.5,length.out=52));
-axis(1, at = seq(0,1, length.out=7), cex.axis=1.2,
+@rput upperE
+@rput lowerE
+R"library(ggplot2); library(cowplot)
 labels = c('Sorenson', 'Richness', 'Mean abun',
-'Geometric mean','Simpson', 'Shannon',  'PD'));
-axis(2, at = seq(0,1, length.out=9), cex.axis=1.2,
-labels = c('Uniform', 'Proportional', 'Largest', 'Rarest', 'Common','Invasive',
-'Phylo invasive','Rand hab \n Loss','Clust hab \n Loss', 'Susceptible'));
-image.plot(meanslope, col = colorRampPalette(brewer.pal(11, 'RdBu'))(51), legend.only=TRUE,
-   breaks =seq(-0.5, 0.5,length.out=52),legend.args=list( text='% change in metric',
-   cex=1.2, side=4,padj = 0.8, line=2), legend.mar = 7)
-mat = expand.grid(seq(0,1, length.out=7), seq(0,1, length.out=9));
-mat = mat[repslope, ]
-points(mat[,1],mat[,2], pch=8, col ='grey20')
-dev.off()
+'Geometric mean','Simpson', 'Shannon',  'PD')
+scenarios = c('Uniform', 'Proportional', 'Largest', 'Rarest', 'Common','Invasive',
+    'Phylo invasive','Rand hab loss', 'Clust hab loss', 'Susceptible')
+dat = data.frame()
+adj = c()
+for (i in 1:7){
+    dat = rbind(dat, data.frame(mn = meanslopeE[i,], up = upperE[i,], lo = lowerE[i,],
+    measure = rep(labels[i], 10), scenario = scenarios, rep = repE[i,]))
+    }
+    for (j in 1:nrow(dat)){
+        dat$adj[j] =  ifelse(dat$mn[j] > 0, dat$up[j] + 0.02, dat$lo[j] - 0.02)
+        }
+    dat$scenario = factor(dat$scenario, levels = c('Uniform', 'Proportional',
+    'Largest', 'Rarest', 'Common','Invasive','Phylo invasive',
+        'Rand hab loss', 'Clust hab loss', 'Susceptible'))
+    g = ggplot(dat, aes(y= mn, x = scenario, fill = measure)) + geom_bar(stat = 'identity') +
+        facet_wrap(~ measure, nrow = 2) + geom_hline(yintercept = 0)+
+        geom_errorbar(aes(ymin=lo, ymax=up),
+                width=.2, position=position_dodge(.9)) + xlab('Scenario of change')+
+                ylab('Mean slope')+theme(legend.position = 'none',
+                axis.text.x = element_text(size=12, angle = 45, vjust = 0.9, hjust = 1))+
+                 geom_text(aes(y = adj, label=rep), size = 6)+
+        scale_fill_manual(palette = colorRampPalette(brewer.pal(7, 'Paired')))
+    png('Early_barplot_trad.png', width = 1200, height = 800)
+    print(g)
+    dev.off()
 "
-
-abunvec = Multinomial(individuals, probs)
-native = fill(true, numSpecies + numInvasive)
-native[numSpecies+numInvasive] = false
-sppl = SpeciesList(numSpecies + numInvasive, 2, abunvec,
-                   energy_vec, movement, param, native, [0.5, 0.5])
-abenv = simplenicheAE(numNiches, grid, totalK, area)
-rel = Match{eltype(abenv.habitat)}()
-eco = Ecosystem(trait_populate!, sppl, abenv, rel)
-mat = Array{Float64, 3}(size(eco.abenv.budget.matrix, 1),size(eco.abenv.budget.matrix, 2), 121)
-for i in 1:121
-    ClustHabitatLoss!(eco, timestep, habloss)
-    mat[:,:,i] = eco.abenv.budget.matrix
-end
-Simulation.reenergise!(eco, totalK, size(eco.abenv.budget.matrix))
-mat2 = Array{Float64, 3}(size(eco.abenv.budget.matrix, 1),size(eco.abenv.budget.matrix, 2), 121)
-for i in 1:121
-    RandHabitatLoss!(eco, timestep, habloss)
-    mat2[:,:,i] = eco.abenv.budget.matrix
-end
-val = reshape(mat[:,:, 121], 2500)
-val2  = reshape(mat2[:,:, 121], 2500)
-@rput val
-@rput val2
-R"library(viridis); library(ggplot2); library(cowplot)
-dat = expand.grid(1:50, 1:50)
-dat$val1 = val
-dat$val2 = val2
-g1 = ggplot(dat, aes(x= Var1, y = Var2)) + geom_tile(aes(fill = factor(val1)))+
-    xlab('') + ylab('')+
-    scale_fill_manual(values = c('tan4', 'palegreen4'),
-        name='Habitat \n status',
-        labels=c('Removed', 'Remaining'))+
-theme(axis.text.x = element_blank(),
-      axis.text.y=element_blank())+
-theme(legend.position = 'right',
-      axis.ticks = element_blank(),
-      axis.line.x = element_blank(),
-      axis.line.y = element_blank())
-g2 = ggplot(dat, aes(x= Var1, y = Var2)) + geom_tile(aes(fill = factor(val2)))+
-xlab('') + ylab('')+
-scale_fill_manual(values = c('palegreen4', 'tan4'),
-    name='Habitat \n status',
-    labels=c('Removed', 'Remaining'))+
-theme(axis.text.x = element_blank(),
-  axis.text.y=element_blank())+
-theme(legend.position = 'none',
-  axis.ticks = element_blank(),
-  axis.line.x = element_blank(),
-  axis.line.y = element_blank())
-g = ggdraw() +
-  draw_plot(g1, 0, 0, 0.525, 0.95) +
-  draw_plot(g2, 0.525, 0, 0.475, 0.95) +
-  draw_plot_label(c('A', 'B'), c(0.01, 0.51), c(0.98, 0.98), size = 18)
-png('Cluster_mat.png', width = 1200, height = 800)
-print(g)
-dev.off()"
 
 rawmat = mapslices(linmod, div[1, :, :, :, :], 2)[:, 1, :, :]
 rawslope = mapslices(mean, rawmat, 3)[:, :, 1] * 10
 sdmat = mapslices(std, rawmat, 3)[:, :, 1]
-upper = rawslope .+ 1.96 .* (sdmat./sqrt(1000))
-lower = rawslope .- 1.96 .* (sdmat./sqrt(1000))
+upper = rawslope .+ sdmat
+lower = rawslope .- sdmat
+maxrep = mapslices(maximum, upper, 2)
+minrep = mapslices(minimum, lower, 2)
+rng = abs(maxrep - minrep)
 @rput rawslope
 @rput upper
 @rput lower
-R"library(ggplot2);library(cowplot)
+@rput rng
+R"library(ggplot2); library(cowplot)
 labels = c('Sorenson', 'Richness', 'Mean abun',
 'Geometric mean','Simpson', 'Shannon',  'PD')
 scenarios = c('Uniform', 'Proportional', 'Largest', 'Rarest', 'Common','Invasive',
-    'Rand hab loss', 'Clust hab loss', 'Susceptible')
+    'Phylo invasive','Rand hab loss', 'Clust hab loss', 'Susceptible')
 dat = data.frame()
+adj = c()
 for (i in 1:7){
     dat = rbind(dat, data.frame(mn = rawslope[i,], up = upper[i,], lo = lower[i,],
-    measure = rep(labels[i], 9), scenario = scenarios))
+    measure = rep(labels[i], 10), scenario = scenarios, rep = rep[i,],
+    rn = rep(rng[i], 10)))
     }
-    dat$scenario = factor(dat$scenario, levels = c('Uniform', 'Proportional', 'Largest', 'Rarest', 'Common','Invasive',
+    for (j in 1:nrow(dat)){
+        dat$adj[j] =  ifelse(dat$mn[j] > 0, dat$up[j] + 0.05 * dat$rn[j],
+        dat$lo[j] - 0.05 * dat$rn[j])
+        }
+    dat$scenario = factor(dat$scenario, levels = c('Uniform', 'Proportional',
+    'Largest', 'Rarest', 'Common','Invasive','Phylo invasive',
         'Rand hab loss', 'Clust hab loss', 'Susceptible'))
-    print(dat)
-    g = ggplot(dat, aes(y= mn, x = scenario, fill = measure)) +
-    geom_bar(stat = 'identity') +
-        facet_wrap(~ measure, nrow = 2,  scales = 'free_y') +
-        geom_hline(yintercept = 0)+
+    g = ggplot(dat, aes(y= mn, x = scenario, fill = measure)) + geom_bar(stat = 'identity') +
+        facet_wrap(~ measure, nrow = 2, scales = 'free_y') + geom_hline(yintercept = 0)+
         geom_errorbar(aes(ymin=lo, ymax=up),
-                width=.2) +
-                xlab('Scenario of change')+
+                width=.2, position=position_dodge(.9)) + xlab('Scenario of change')+
                 ylab('Mean slope')+theme(legend.position = 'none',
-                axis.text.x = element_text(size=12, angle = 45,
-                 vjust = 0.9, hjust = 1))
-    png('barplot_trad_raw.png', width = 1200, height = 800)
+                axis.text.x = element_text(size=12, angle = 45, vjust = 0.9, hjust = 1))+
+                 geom_text(aes(y = adj, label=rep), size = 6)+
+        scale_fill_manual(palette = colorRampPalette(brewer.pal(7, 'Paired')))
+    png('Raw_barplot_trad.png', width = 1200, height = 800)
+    print(g)
+    dev.off()
+"
+# Check for early and late warning signals
+early = div[:,:, 1:24,:,:]
+
+slopematE = mapslices(linmod, early[1, :, :, :, :], 2)[:, 1, :, :]
+meanslopeE = mapslices(mean, slopematE, 3)[:, :, 1]
+sdmatE = mapslices(std, slopematE, 3)[:, :, 1]
+upperE = meanslopeE .+ sdmatE
+lowerE = meanslopeE .- sdmatE
+repE = Array{String, 2}(7, 10)
+for i in 1:7
+    for j in 1:10
+    if sum(slopematE[i,j,:] .> 0)/1000 >= 0.95 || sum(slopematE[i,j,:] .< 0)/1000 >= 0.95
+        repE[i, j ] = "*"
+    else
+        repE[i, j] = ""
+    end
+end
+end
+maxrepE = mapslices(maximum, upperE, 2)
+minrepE = mapslices(minimum, lowerE, 2)
+rngE = abs(maxrepE - minrepE)
+@rput repE
+@rput meanslopeE
+@rput upperE
+@rput lowerE
+@rput rngE
+R"library(ggplot2); library(cowplot)
+labels = c('Sorenson', 'Richness', 'Mean abun',
+'Geometric mean','Simpson', 'Shannon',  'PD')
+scenarios = c('Uniform', 'Proportional', 'Largest', 'Rarest', 'Common','Invasive',
+    'Phylo invasive','Rand hab loss', 'Clust hab loss', 'Susceptible')
+dat = data.frame()
+adj = c()
+for (i in 1:7){
+    dat = rbind(dat, data.frame(mn = meanslopeE[i,], up = upperE[i,], lo = lowerE[i,],
+    measure = rep(labels[i], 10), scenario = scenarios, rep = repE[i,],
+    rn = rep(rngE[i], 10)))
+    }
+    for (j in 1:nrow(dat)){
+        dat$adj[j] =  ifelse(dat$mn[j] > 0, dat$up[j] + 0.05 * dat$rn[j],
+        dat$lo[j] - 0.05 * dat$rn[j])
+        }
+    dat$scenario = factor(dat$scenario, levels = c('Uniform', 'Proportional',
+    'Largest', 'Rarest', 'Common','Invasive','Phylo invasive',
+        'Rand hab loss', 'Clust hab loss', 'Susceptible'))
+    g = ggplot(dat, aes(y= mn, x = scenario, fill = measure)) + geom_bar(stat = 'identity') +
+        facet_wrap(~ measure, nrow = 2, scales='free_y') + geom_hline(yintercept = 0)+
+        geom_errorbar(aes(ymin=lo, ymax=up),
+                width=.2, position=position_dodge(.9)) + xlab('Scenario of change')+
+                ylab('Mean slope')+theme(legend.position = 'none',
+                axis.text.x = element_text(size=12, angle = 45, vjust = 0.9, hjust = 1))+
+                 geom_text(aes(y = adj, label=rep), size = 6)+
+        scale_fill_manual(palette = colorRampPalette(brewer.pal(7, 'Paired')))
+    png('Early_raw_barplot_trad.png', width = 1200, height = 800)
     print(g)
     dev.off()
 "
