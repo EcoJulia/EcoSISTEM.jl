4bcc8b523b28b6ef0f6390b7bd5896218d91513a
Claire Harris <c.harris.2@research.gla.ac.uk>
2018-11-26 12:33:56 +0000

    Santini_static_reeve.jl: Remove extra code for phylo rerooting (only need this in the branch-based measures) and change plots to be in line with traditional measures.

--- examples/Santini_static_reeve.jl
+++ examples/Santini_static_reeve.jl
@@ -82,12 +82,10 @@ function runsim(times::Unitful.Time)
             size_var, area, movement, param, native, [0.5, 0.5])
             Simulation.resettraits!(sppl.types.tree)
             sppl.susceptible = ContinuousEvolve(sus_mean, sus_var, sppl.types.tree).mean
-            if i == 7
-                reroot!(sppl.types.tree, "151")
-            end
             abenv = simplenicheAE(numNiches, grid, totalK, area)
             rel = Match{eltype(abenv.habitat)}()
             eco = Ecosystem(trait_populate!, sppl, abenv, rel)
+            eco = makeunique(eco)
             thisabun = view(abun, :, :, :, i, j);
             simulate_record_diversity!(thisabun, eco, times, interval, timestep,
             scenario[i], divfuns, q)
@@ -112,31 +110,7 @@ function linmod(x)
 end
 slopemat = mapslices(linmod, stanmat[1, :, :, :, :], 2)[:, 1, :, :]
 meanslope = mapslices(mean, slopemat, 3)[:, :, 1] * 10
-repslope = mapslices(x-> (sum(x .> 0)/length(x)) > 0.95 || (sum(x .< 0)/length(x)) > 0.95, slopemat, 3)[:, :, 1]
-repslope = reshape(repslope, 63, 1)
-@rput meanslope
-@rput repslope
-R"
-library(fields);
-library(viridis);library(RColorBrewer)
-png('Meanslope_static150_reeve.png', width = 1000, height = 900)
-par(mfrow=c(1,1), mar=c(6,4,4,10));
-image(meanslope, axes=FALSE, xlab='', ylab='', srt=45, col = colorRampPalette(brewer.pal(11, 'RdBu'))(51),
-   breaks =seq(-0.5, 0.5,length.out=52));
-axis(1, at = seq(0,1, length.out=7), cex.axis=1.2,
-labels = c('Raw alpha', 'Norm alpha', 'Raw beta',
-'Norm beta','Raw rho', 'Norm rho',  'Gamma'));
-axis(2, at = seq(0,1, length.out=9), cex.axis=1.2,
-labels = c('Uniform', 'Proportional', 'Largest', 'Rarest', 'Common','Invasive', 'Rand hab \n Loss',
-'Clust hab \n Loss', 'Susceptible'));
-image.plot(meanslope, col = colorRampPalette(brewer.pal(11, 'RdBu'))(51), legend.only=TRUE,
-   breaks =seq(-0.5, 0.5,length.out=52),legend.args=list( text='% change in metric',
-   cex=1.2, side=4,padj = 0.8, line=2), legend.mar = 7)
-mat = expand.grid(seq(0,1, length.out=7), seq(0,1, length.out=9));
-mat = mat[repslope, ]
-points(mat[,1],mat[,2], pch=8, col ='grey20')
-dev.off()
-"
+
 # Plot trends over time
 @rput stanmat
 standat = DataFrame(div = reshape(stanmat[1,:,:,1,:], 847000))
@@ -148,9 +122,28 @@ standat[:rep] = vcat(map(x -> repmat([x], 847), 1:1000)...)
 @rput standat
 R"library(ggplot2); library(cowplot); library(scales)
 png('Temp_trends_reeve.png', width = 1000, height = 900)
+standat$measure = factor(standat$measure, levels = c('Raw alpha', 'Norm alpha',
+'Raw beta', 'Norm beta', 'Raw rho', 'Norm rho','Gamma'))
 g = ggplot(data = standat, aes(x=time, y = div, group = rep))+
-geom_line(col = alpha('black', 0.1))+facet_wrap(~measure)+ylim(-5, 5)+
+geom_line(col = alpha('black', 0.1))+facet_wrap(~measure, nrow =2)+ylim(-5, 5)+
 xlab('Time (years)') + ylab('Diversity value')
+print(g);dev.off()
+"
+
+standat = DataFrame(div = reshape(div[1,1,:,:,:], 1210000))
+standat[:time] = (repmat(collect(1:121), 10000).-1)./12
+#standat[:time] = (repmat(vcat(map(x -> repmat([x], 9), 1:121)...), 1000) .- 1)./12
+standat[:scenario] = repmat(vcat(map(x -> repmat([x], 121), ["Uniform", "Proportional", "Large", "Rare",
+    "Common", "Invasive","Phylo invasive", "Rand hab", "Clust hab", "Susceptible"])...), 1000)
+standat[:rep] = vcat(map(x -> repmat([x], 1210), 1:1000)...)
+@rput standat
+R"library(ggplot2); library(cowplot); library(scales)
+standat$scenario = factor(standat$scenario, levels = c('Uniform', 'Proportional',
+'Large', 'Rare', 'Common', 'Invasive','Phylo invasive', 'Rand hab', 'Clust hab', 'Susceptible'))
+png('Alpha_trends_reeve.png', width = 1000, height = 900)
+g = ggplot(data = standat, aes(x=time, y = div, group = rep))+
+geom_line(col = alpha('black', 0.1))+facet_wrap(~scenario, nrow =2)+
+xlab('Time (years)') + ylab('Scenario')
 print(g);dev.off()
 "
 
@@ -170,74 +163,49 @@ theme(axis.text = element_text(size =16), axis.title = element_text(size =18),
 strip.text = element_text(size=18))
 print(g);dev.off()
 "
-sdmat = mapslices(std, slopemat, 3)[:, :, 1]
-upper = meanslope .+ 1.96 .* (sdmat./sqrt(1000))
-lower = meanslope .- 1.96 .* (sdmat./sqrt(1000))
+sdmat = mapslices(std, slopemat, 3)[:, :, 1] * 10
+upper = meanslope .+ 2.24 .* (sdmat./sqrt(1000))
+lower = meanslope .- 2.24 .* (sdmat./sqrt(1000))
+reps = Array{String, 2}(7, 10)
+for i in 1:7
+    for j in 1:10
+    if sum(slopemat[i,j,:] .> 0)/1000 >= 0.95 || sum(slopemat[i,j,:] .< 0)/1000 >= 0.95
+        reps[i, j ] = ifelse(meanslope[i,j] > 0, "+", "-")
+    else
+        reps[i, j] = ""
+    end
+end
+end
 @rput meanslope
 @rput upper
 @rput lower
-R"library(ggplot2)
+@rput reps
+R"library(ggplot2); library(cowplot); library(RColorBrewer)
 labels = c('Raw alpha', 'Norm alpha', 'Raw beta',
 'Norm beta','Raw rho', 'Norm rho',  'Gamma')
 scenarios = c('Uniform', 'Proportional', 'Largest', 'Rarest', 'Common','Invasive',
-'Rand hab loss', 'Clust hab loss', 'Susceptible')
+    'Phylo invasive','Rand hab loss', 'Clust hab loss', 'Susceptible')
 dat = data.frame()
+adj = c()
 for (i in 1:7){
     dat = rbind(dat, data.frame(mn = meanslope[i,], up = upper[i,], lo = lower[i,],
-    measure = rep(labels[i], 9), scenario = scenarios))
+    measure = rep(labels[i], 10), scenario = scenarios, rep = reps[i,]))
     }
-    dat$scenario = factor(dat$scenario, levels = c('Uniform', 'Proportional', 'Largest', 'Rarest', 'Common','Invasive',
+    for (j in 1:nrow(dat)){
+        dat$adj[j] =  ifelse(dat$mn[j] > 0, dat$mn[j] + 0.02, dat$mn[j] - 0.02)
+        }
+    dat$scenario = factor(dat$scenario, levels = c('Uniform', 'Proportional',
+    'Largest', 'Rarest', 'Common','Invasive','Phylo invasive',
         'Rand hab loss', 'Clust hab loss', 'Susceptible'))
-    print(dat)
     g = ggplot(dat, aes(y= mn, x = scenario, fill = measure)) + geom_bar(stat = 'identity') +
         facet_wrap(~ measure, nrow = 2) + geom_hline(yintercept = 0)+
         geom_errorbar(aes(ymin=lo, ymax=up),
                 width=.2, position=position_dodge(.9)) + xlab('Scenario of change')+
                 ylab('Mean slope')+theme(legend.position = 'none',
-                axis.text.x = element_text(size=12, angle = 45, vjust = 0.9, hjust = 1))
+                axis.text.x = element_text(size=12, angle = 45, vjust = 0.9, hjust = 1))+
+                 geom_text(aes(y = adj, label=rep), size = 6)+
+        scale_fill_manual(palette = colorRampPalette(brewer.pal(7, 'Paired')))
     png('barplot_reeve.png', width = 1200, height = 800)
-    print(g)
-    dev.off()
-"
-
-
-function linmod(x)
-    df = DataFrame(X = x, Y = 1:length(x))
-    mod = GLM.lm(@formula(X ~ Y), df)
-    return coef(mod)[2]
-end
-rawmat = mapslices(linmod, div[1, :, :, :, :], 2)[:, 1, :, :]
-rawslope = mapslices(mean, rawmat, 3)[:, :, 1] * 10
-sdmat = mapslices(std, rawmat, 3)[:, :, 1]
-upper = rawslope .+ 1.96 .* (sdmat./sqrt(1000))
-lower = rawslope .- 1.96 .* (sdmat./sqrt(1000))
-@rput rawslope
-@rput upper
-@rput lower
-R"library(ggplot2)
-labels = c('Raw alpha', 'Norm alpha', 'Raw beta',
-'Norm beta','Raw rho', 'Norm rho',  'Gamma')
-scenarios = c('Uniform', 'Proportional', 'Largest', 'Rarest', 'Common','Invasive',
-    'Rand hab loss', 'Clust hab loss', 'Susceptible')
-dat = data.frame()
-for (i in 1:7){
-    dat = rbind(dat, data.frame(mn = rawslope[i,], up = upper[i,], lo = lower[i,],
-    measure = rep(labels[i], 9), scenario = scenarios))
-    }
-    dat$scenario = factor(dat$scenario, levels = c('Uniform', 'Proportional', 'Largest', 'Rarest', 'Common','Invasive',
-        'Rand hab loss', 'Clust hab loss', 'Susceptible'))
-    print(dat)
-    g = ggplot(dat, aes(y= mn, x = scenario, fill = measure)) +
-    geom_bar(stat = 'identity') +
-        facet_wrap(~ measure, nrow = 2,  scales = 'free_y') +
-        geom_hline(yintercept = 0)+
-        geom_errorbar(aes(ymin=lo, ymax=up),
-                width=.2) +
-                xlab('Scenario of change')+
-                ylab('Mean slope')+theme(legend.position = 'none',
-                axis.text.x = element_text(size=12, angle = 45,
-                 vjust = 0.9, hjust = 1))
-    png('barplot_reeve_raw.png', width = 1200, height = 800)
     print(g)
     dev.off()
 "
@@ -245,55 +213,153 @@ for (i in 1:7){
 
 # Check for early and late warning signals
 early = stanmat[:,:, 1:24,:,:]
-late = stanmat[:,:, 25:end,:,:]
 
 slopematE = mapslices(linmod, early[1, :, :, :, :], 2)[:, 1, :, :]
 meanslopeE = mapslices(mean, slopematE, 3)[:, :, 1]
-repslopeE = mapslices(x-> (sum(x .> 0)/length(x)) > 0.95 || (sum(x .< 0)/length(x)) > 0.95, slopematE, 3)[:, :, 1]
-repslopeE = reshape(repslopeE, 63, 1)
-
-slopematL = mapslices(linmod, late[1, :, :, :, :], 2)[:, 1, :, :]
-meanslopeL = mapslices(mean, slopematL, 3)[:, :, 1]
-repslopeL = mapslices(x-> (sum(x .> 0)/length(x)) > 0.95 || (sum(x .< 0)/length(x)) > 0.95, slopematL, 3)[:, :, 1]
-repslopeL = reshape(repslopeL, 63, 1)
-
-meanslopeT = Array{Float64, 2}(18, 7)
-for i in 1:9
-    meanslopeT[(2*i - 1), :] = meanslopeE[:, i]
-    meanslopeT[(2*i), :] = meanslopeL[:, i]
+sdmatE = mapslices(std, slopematE, 3)[:, :, 1]
+upperE = meanslopeE .+ 2.24 .* (sdmatE./sqrt(1000))
+lowerE = meanslopeE .- 2.24 .* (sdmatE./sqrt(1000))
+repE = Array{String, 2}(7, 10)
+for i in 1:7
+    for j in 1:10
+    if sum(slopematE[i,j,:] .> 0)/1000 >= 0.95 || sum(slopematE[i,j,:] .< 0)/1000 >= 0.95
+        repE[i, j ] = ifelse(meanslopeE[i,j] > 0, "+", "-")
+    else
+        repE[i, j] = ""
+    end
 end
-meanslopeT *= 10
-repslopeT = Array{Bool, 2}(63*2, 1)
-for i in 1:63
-    repslopeT[(2*i - 1), :] = repslopeE[i, :]
-    repslopeT[(2*i), :] = repslopeL[i, :]
 end
-@rput meanslopeT
-@rput repslopeT
-R"
-library(fields);
-library(viridis);library(RColorBrewer)
-png('EWS_static150_reeve.png', width = 1000, height = 1200)
-par(mfrow=c(1,1), mar=c(6,6,4,8));
-metriclabels = c('Raw alpha', 'Norm alpha', 'Raw beta',
-'Norm beta','Raw rho', 'Norm rho',  'Gamma');
+@rput repE
+@rput meanslopeE
+@rput upperE
+@rput lowerE
+R"library(ggplot2); library(cowplot)
+labels = c('Raw alpha', 'Norm alpha', 'Raw beta',
+'Norm beta','Raw rho', 'Norm rho',  'Gamma')
+scenarios = c('Uniform', 'Proportional', 'Largest', 'Rarest', 'Common','Invasive',
+    'Phylo invasive','Rand hab loss', 'Clust hab loss', 'Susceptible')
+dat = data.frame()
+adj = c()
+for (i in 1:7){
+    dat = rbind(dat, data.frame(mn = meanslopeE[i,], up = upperE[i,], lo = lowerE[i,],
+    measure = rep(labels[i], 10), scenario = scenarios, rep = repE[i,]))
+    }
+    for (j in 1:nrow(dat)){
+        dat$adj[j] =  ifelse(dat$mn[j] > 0, dat$up[j] + 0.01, dat$lo[j] - 0.01)
+        }
+    dat$scenario = factor(dat$scenario, levels = c('Uniform', 'Proportional',
+    'Largest', 'Rarest', 'Common','Invasive','Phylo invasive',
+        'Rand hab loss', 'Clust hab loss', 'Susceptible'))
+    g = ggplot(dat, aes(y= mn, x = scenario, fill = measure)) + geom_bar(stat = 'identity') +
+        facet_wrap(~ measure, nrow = 2) + geom_hline(yintercept = 0)+
+        geom_errorbar(aes(ymin=lo, ymax=up),
+                width=.2, position=position_dodge(.9)) + xlab('Scenario of change')+
+                ylab('Mean slope')+theme(legend.position = 'none',
+                axis.text.x = element_text(size=12, angle = 45, vjust = 0.9, hjust = 1))+
+                 geom_text(aes(y = adj, label=rep), size = 6)+
+        scale_fill_manual(palette = colorRampPalette(brewer.pal(7, 'Paired')))
+    png('Early_barplot_reeve.png', width = 1200, height = 800)
+    print(g)
+    dev.off()
+"
 
-image(t(meanslopeT), axes=FALSE, xlab='', ylab='', srt=45, col = colorRampPalette(brewer.pal(11, 'RdBu'))(51),
-    breaks =seq(-1.3, 1.3,length.out=52));
-axis(1, at = seq(0,1, length.out=7), metriclabels, cex = 1.2);
-labels = c('Uniform \n \n', 'Proportional \n \n', 'Largest \n \n', 'Rarest \n \n',
- 'Common \n \n','Invasive \n \n', 'Rand Hab Loss \n \n', 'Clust Hab Loss \n \n', 'Susceptible \n \n')
-sublabels = c('\n EWS', '\nLWS')
-#labels = paste(expand.grid(sublabels, labels)[,1],
-#    expand.grid(sublabels, labels)[,2])
-axis(2, at = seq(0,1, length.out=18), rep(sublabels, 9));
-axis(2, at = seq(0.04, 1., 0.115), labels, tick = F, font =2, cex = 1.2);
-#text(x = -0.5, y = seq(0.05, 1, 0.15), labels, srt = 90)
-image.plot(meanslopeT, col = colorRampPalette(brewer.pal(11, 'RdBu'))(51), legend.only=TRUE,
-    breaks =seq(-1.3, 1.3,length.out=52), legend.args=list( text='% change in metric',
-    cex=1.2, side=4,padj = 0.8, line=2), legend.mar = 7)
-mat = expand.grid(seq(0,1, length.out=7), seq(0,1, length.out=18));
-mat = mat[repslopeT, ]
-points(mat[,1],mat[,2], pch=8, col ='grey20')
-dev.off()
+rawmat = mapslices(linmod, div[1, :, :, :, :], 2)[:, 1, :, :]
+rawslope = mapslices(mean, rawmat, 3)[:, :, 1] * 10
+sdmat = mapslices(std, rawmat, 3)[:, :, 1]
+upper = rawslope .+ 2.24 .* (sdmat./sqrt(1000))
+lower = rawslope .- 2.24 .* (sdmat./sqrt(1000))
+maxrep = mapslices(maximum, upper, 2)
+minrep = mapslices(minimum, lower, 2)
+rng = abs(maxrep - minrep)
+@rput rawslope
+@rput upper
+@rput lower
+@rput rng
+R"library(ggplot2); library(cowplot)
+labels = c('Raw alpha', 'Norm alpha', 'Raw beta',
+'Norm beta','Raw rho', 'Norm rho',  'Gamma')
+scenarios = c('Uniform', 'Proportional', 'Largest', 'Rarest', 'Common','Invasive',
+    'Phylo invasive','Rand hab loss', 'Clust hab loss', 'Susceptible')
+dat = data.frame()
+adj = c()
+for (i in 1:7){
+    dat = rbind(dat, data.frame(mn = rawslope[i,], up = upper[i,], lo = lower[i,],
+    measure = rep(labels[i], 10), scenario = scenarios, rep = reps[i,],
+    rn = rep(rng[i], 10)))
+    }
+    for (j in 1:nrow(dat)){
+        dat$adj[j] =  ifelse(dat$mn[j] > 0, dat$up[j] + 0.05 * dat$rn[j],
+        dat$lo[j] - 0.05 * dat$rn[j])
+        }
+    dat$scenario = factor(dat$scenario, levels = c('Uniform', 'Proportional',
+    'Largest', 'Rarest', 'Common','Invasive','Phylo invasive',
+        'Rand hab loss', 'Clust hab loss', 'Susceptible'))
+    g = ggplot(dat, aes(y= mn, x = scenario, fill = measure)) + geom_bar(stat = 'identity') +
+        facet_wrap(~ measure, nrow = 2, scales = 'free_y') + geom_hline(yintercept = 0)+
+        geom_errorbar(aes(ymin=lo, ymax=up),
+                width=.2, position=position_dodge(.9)) + xlab('Scenario of change')+
+                ylab('Mean slope')+theme(legend.position = 'none',
+                axis.text.x = element_text(size=12, angle = 45, vjust = 0.9, hjust = 1))+
+                 geom_text(aes(y = adj, label=rep), size = 6)+
+        scale_fill_manual(palette = colorRampPalette(brewer.pal(7, 'Paired')))
+    png('Raw_barplot_reeve.png', width = 1200, height = 800)
+    print(g)
+    dev.off()
+"
+# Check for early and late warning signals
+early = div[:,:, 1:24,:,:]
+
+slopematE = mapslices(linmod, early[1, :, :, :, :], 2)[:, 1, :, :]
+meanslopeE = mapslices(mean, slopematE, 3)[:, :, 1]
+sdmatE = mapslices(std, slopematE, 3)[:, :, 1]
+upperE = meanslopeE .+ 2.24 .* (sdmatE./sqrt(1000))
+lowerE = meanslopeE .- 2.24 .* (sdmatE./sqrt(1000))
+repE = Array{String, 2}(7, 10)
+for i in 1:7
+    for j in 1:10
+    if sum(slopematE[i,j,:] .> 0)/1000 >= 0.95 || sum(slopematE[i,j,:] .< 0)/1000 >= 0.95
+        repE[i, j ] = ifelse(meanslopeE[i,j] > 0, "+", "-")
+    else
+        repE[i, j] = ""
+    end
+end
+end
+maxrepE = mapslices(maximum, upperE, 2)
+minrepE = mapslices(minimum, lowerE, 2)
+rngE = abs(maxrepE - minrepE)
+@rput repE
+@rput meanslopeE
+@rput upperE
+@rput lowerE
+@rput rngE
+R"library(ggplot2); library(cowplot)
+labels = c('Raw alpha', 'Norm alpha', 'Raw beta',
+'Norm beta','Raw rho', 'Norm rho',  'Gamma')
+scenarios = c('Uniform', 'Proportional', 'Largest', 'Rarest', 'Common','Invasive',
+    'Phylo invasive','Rand hab loss', 'Clust hab loss', 'Susceptible')
+dat = data.frame()
+adj = c()
+for (i in 1:7){
+    dat = rbind(dat, data.frame(mn = meanslopeE[i,], up = upperE[i,], lo = lowerE[i,],
+    measure = rep(labels[i], 10), scenario = scenarios, rep = repE[i,],
+    rn = rep(rngE[i], 10)))
+    }
+    for (j in 1:nrow(dat)){
+        dat$adj[j] =  ifelse(dat$mn[j] > 0, dat$up[j] + 0.05 * dat$rn[j],
+        dat$lo[j] - 0.05 * dat$rn[j])
+        }
+    dat$scenario = factor(dat$scenario, levels = c('Uniform', 'Proportional',
+    'Largest', 'Rarest', 'Common','Invasive','Phylo invasive',
+        'Rand hab loss', 'Clust hab loss', 'Susceptible'))
+    g = ggplot(dat, aes(y= mn, x = scenario, fill = measure)) + geom_bar(stat = 'identity') +
+        facet_wrap(~ measure, nrow = 2, scales='free_y') + geom_hline(yintercept = 0)+
+        geom_errorbar(aes(ymin=lo, ymax=up),
+                width=.2, position=position_dodge(.9)) + xlab('Scenario of change')+
+                ylab('Mean slope')+theme(legend.position = 'none',
+                axis.text.x = element_text(size=12, angle = 45, vjust = 0.9, hjust = 1))+
+                 geom_text(aes(y = adj, label=rep), size = 6)+
+        scale_fill_manual(palette = colorRampPalette(brewer.pal(7, 'Paired')))
+    png('Early_raw_barplot_reeve.png', width = 1200, height = 800)
+    print(g)
+    dev.off()
 "
