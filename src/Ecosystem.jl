e5a75cfbaf903b09979ac2364fec00446e6b80cf
Claire Harris <c.harris.2@research.gla.ac.uk>
2018-12-13 14:21:52 +0000

    Ecosystem.jl: Add TestEcosystem() type for testing of Simulation package - simple example.

--- src/Ecosystem.jl
+++ src/Ecosystem.jl
@@ -142,6 +142,37 @@ function Ecosystem(spplist::SpeciesList, abenv::GridAbioticEnv,
    return Ecosystem(populate!, spplist, abenv, rel)
 end
 
+function TestEcosystem()
+    numSpecies = 150
+    numNiches = 2
+
+    birth = 0.0/month
+    death = 0.0/month
+    long = 1.0
+    surv = 0.0
+    boost = 1000.0
+    timestep = 1.0month
+    param = EqualPop(birth, death, long, surv, boost)
+
+    grid = (50, 50)
+    area = 10000.0km^2
+    individuals=20000 * numSpecies
+    totalK = 1000000.0 * numSpecies
+    abenv = simplenicheAE(numNiches, grid, totalK, area)
+
+    abun = rand(Multinomial(individuals, numSpecies))
+
+    kernel = GaussianKernel(1.0km, numSpecies, 10e-04)
+    movement = BirthOnlyMovement(kernel)
+    native = fill(true, numSpecies)
+    energy = SimpleRequirement(fill(2.0, numSpecies))
+    sppl = SpeciesList(numSpecies, numNiches, abun, energy, movement, param, native)
+
+    rel = Match{eltype(abenv.habitat)}()
+    eco = Ecosystem(trait_populate!, sppl, abenv, rel)
+    return eco
+end
+
 """
     CachedEcosystem{Part <: AbstractAbiotic, SL <: SpeciesList,
         TR <: AbstractTraitRelationship} <: AbstractEcosystem{Part, SL, TR}
