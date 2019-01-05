4d15f894305307cb1261b0c5243146bb7201923d
Claire Harris <c.harris.2@research.gla.ac.uk>
2019-01-05 22:15:03 +0000

    runtests.jl: Change how replace works and replace ismatch with occursin to update test running to 1.0.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,19 @@
 # Identify files in test/ that are testing matching files in src/
 #  - src/Source.jl will be matched by test/test_Source.jl
-include("TestCases.jl")
-filebase = map(file -> replace(file, r"(.*).jl", s"\1"),
-                filter(file -> ismatch(r".*\.jl", file), readdir("../src")))
-testbase = map(file -> replace(file, r"test_(.*).jl", s"\1"),
-                filter(str -> ismatch(r"^test_.*\.jl$", str), readdir()))
+using Compat
 
-info("Running tests for files:")
+filebase = map(file -> replace(file, r"(.*).jl" => s"\1"),
+                filter(file -> occursin(r".*\.jl", file), readdir("../src")))
+testbase = map(file -> replace(file, r"test_(.*).jl" => s"\1"),
+                filter(str -> occursin(r"^test_.*\.jl$", str), readdir()))
+
+@info "Running tests for files:"
 for t in testbase
     println("    = $t.jl")
 end
 println()
 
-info("Running tests...")
-for t in testbase
+@info "Running tests..."
     fn = "test_$t.jl"
     println("    * Testing $t.jl ...")
     include(fn)
