58a69dec844c2f3a6f648a2903cc202a6e52f92e
Richard Reeve <git@richardreeve.net>
2020-02-16 12:33:35 +0000

    Matching MPI and non-MPI examples.

--- examples/CirrusSmallNonMPIRun.jl
+++ examples/CirrusSmallNonMPIRun.jl
@@ -5,11 +5,8 @@ using Simulation
 using MyUnitful
 using Unitful, Unitful.DefaultSymbols
 using Distributions
-using MPI
 
-MPI.Init()
-comm = MPI.COMM_WORLD
-rank = MPI.Comm_rank(comm)
+rank = 0
 println(Threads.nthreads())
 numSpecies = 100; grid = (50, 50); req= 10.0kJ; individuals=1_000_000; area = 100000.0*km^2; totalK = 100.0kJ/km^2
 # Set up initial parameters for ecosystem
@@ -48,17 +45,16 @@ abenv = simplehabitatAE(274.0K, grid, totalK, area)
 rel = Gauss{typeof(1.0K)}()
 
 # Create ecosystem
-eco = MPIEcosystem(sppl, abenv, rel)
+eco = Ecosystem(sppl, abenv, rel)
 
 sleep(rank)
 print("$(rank):")
-println(sum(eco.abundances.rows_matrix, dims = 2))
+println(sum(eco.abundances.matrix, dims = 2))
 
 # Simulation Parameters
 burnin = 20years; times = 50years; timestep = 1month; record_interval = 3months; repeats = 1
 lensim = length(0years:record_interval:times)
 # Burnin
-MPI.Barrier(comm)
 one = time_ns()
 @time simulate!(eco, burnin, timestep)
 two = time_ns()
@@ -67,5 +63,4 @@ sleep(rank + 1)
 println("$(rank): $((two - one) / 10 ^ 9)")
 
 print("$(rank):")
-println(sum(eco.abundances.rows_matrix, dims = 2))
-MPI.Finalize()
+println(sum(eco.abundances.matrix, dims = 2))
