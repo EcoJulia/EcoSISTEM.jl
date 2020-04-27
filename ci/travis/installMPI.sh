3aff321aeac88b74e90517b56a183bcbb5a21019
Alex Robson <alex.robson@invenialabs.co.uk>
2020-04-27 11:40:12 +0100

    Corrections to path of openMPI

--- ci/travis/installMPI.sh
+++ ci/travis/installMPI.sh
@@ -8,23 +8,23 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then
 	echo "Using cached OpenMPI"
     else
 	echo "Installing OpenMPI with homebrew"
-				brew install open-mpi
-				ln -s /usr/local/bin bin
-				ln -s /usr/local/lib lib
-				ln -s /usr/local/include include
+			brew install open-mpi
+			ln -s /usr/local/bin bin
+			ln -s /usr/local/lib lib
+			ln -s /usr/local/include include
     fi
 else
-    if [ -f "openmpi/bin/mpirun" ] && [ -f "openmpi-2.0.1/config.log" ]; then
+    if [ -f "openmpi/bin/mpirun" ] && [ -f "openmpi-4.0.3/config.log" ]; then
 			echo "Using cached OpenMPI"
 			echo "Configuring OpenMPI"
-			cd openmpi-2.0.1
+			cd openmpi-4.0.3
 			./configure --prefix=$TRAVIS_BUILD_DIR/openmpi CC=$C_COMPILER CXX=$CXX_COMPILER &> openmpi.configure
-		    else
+		else
 			echo "Downloading OpenMPI Source"
 			wget https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-4.0.3.tar.gz
-			tar zxf openmpi-2.0.1.tar.gz
+			tar zxf openmpi-4.0.3.tar.gz
 			echo "Configuring and building OpenMPI"
-			cd openmpi-2.0.1
+			cd openmpi-4.0.3
 			./configure --prefix=$TRAVIS_BUILD_DIR/openmpi CC=$C_COMPILER CXX=$CXX_COMPILER &> openmpi.configure
 			make -j4 &> openmpi.make
 			make install &> openmpi.install
