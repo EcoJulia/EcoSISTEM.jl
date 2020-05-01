2febb6d0d47e85cede276ff2f35f8ae42bf10a24
Eric Perim <eric.perim@invenialabs.co.uk>
2020-05-01 19:03:39 +0100

    Save simulation inputs and outputs

--- src/Epidemiology/EpiHelper.jl
+++ src/Epidemiology/EpiHelper.jl
@@ -2,20 +2,83 @@ using Unitful
 using Unitful.DefaultSymbols
 using Simulation.Units
 
-function simulate!(epi::AbstractEpiSystem, duration::Unitful.Time, timestep::Unitful.Time)
+"""
+    simulate!(
+        epi::AbstractEpiSystem,
+        duration::Unitful.Time,
+        interval::Unitful.Time,
+        timestep::Unitful.Time;
+        save=false,
+        save_path=pwd(),
+    )
+
+Run an epidemiological system, `epi`, for specified length of times, `duration`, for a
+particular timestep, `timestep`. If `save=true`, inputs and outputs are saved as JLSO files
+at `save_path`.
+"""
+function simulate!(
+    epi::AbstractEpiSystem,
+    duration::Unitful.Time,
+    timestep::Unitful.Time;
+    save=false,
+    save_path=pwd(),
+)
   times = length(0s:timestep:duration)
+  if save && !isdir(save_path) # Create the directory if it doesn't already exist.
+      mkdir(save_path)
+  end
+  save && Simulation.save(joinpath(save_path, "initial_system.jlso"), epi)
+  save && JLSO.save(
+    joinpath(save_path, "configuration.jlso"),
+    :duration => duration,
+    :timestep => timestep,
+  )
   for i in 1:times
     update!(epi, timestep)
   end
+  save && Simulation.save(joinpath(save_path, "final_system.jlso"), epi)
 end
 
-function simulate_record!(storage::AbstractArray, epi::EpiSystem,
-  times::Unitful.Time, interval::Unitful.Time,timestep::Unitful.Time)
+"""
+    simulate_record!(
+        storage::AbstractArray,
+        epi::EpiSystem,
+        times::Unitful.Time,
+        interval::Unitful.Time,
+        timestep::Unitful.Time;
+        save=false,
+        save_path=pwd(),
+    )
+
+Run an epidemiological system, `epi`, for specified length of times, `times`,
+for a particular timestep, `timestep`, and time interval for abundances to be
+recorded, `interval`. Optionally, there may also be a scenario by which the
+whole ecosystem is updated, such as removal of habitat patches.
+"""
+function simulate_record!(
+    storage::AbstractArray,
+    epi::EpiSystem,
+    times::Unitful.Time,
+    interval::Unitful.Time,
+    timestep::Unitful.Time;
+    save=false,
+    save_path=pwd(),
+)
   ustrip(mod(interval,timestep)) == 0.0 || error("Interval must be a multiple of timestep")
   record_seq = 0s:interval:times
   time_seq = 0s:timestep:times
   storage[:, :, 1] = epi.abundances.matrix
   counting = 1
+  if save && !isdir(save_path) # Create the directory if it doesn't already exist.
+      mkdir(save_path)
+  end
+  save && Simulation.save(joinpath(save_path, "initial_system.jlso"), epi)
+  save && JLSO.save(
+    joinpath(save_path, "configuration.jlso"),
+    :times => times,
+    :timestep => timestep,
+    :interval => interval,
+  )
   for i in 2:length(time_seq)
     update!(epi, timestep);
     if time_seq[i] in record_seq
@@ -24,5 +87,7 @@ function simulate_record!(storage::AbstractArray, epi::EpiSystem,
     end
     print(".")
   end
-  storage
+  save && Simulation.save(joinpath(save_path, "final_system.jlso"), epi)
+  save && JLSO.save(joinpath(save_path, "abundances.jlso"), :abundances => storage)
+  return storage
 end
--- test/test_EpiHelper.jl
+++ test/test_EpiHelper.jl
@@ -8,14 +8,32 @@ include("TestCases.jl")
 
 epi = TestEpiSystem()
 
-times = 1.0month; burnin = 1.0month; interval = 1.0day
-timestep = 1.0day
-# Run simulation grid
-lensim = length(0.0month:interval:times)
+@testset "EpiHelper" begin
+    times = 1.0month; burnin = 1.0month; interval = 1.0day
+    timestep = 1.0day
+    # Run simulation grid
+    lensim = length(0.0month:interval:times)
 
-# Run simulations 10 times
-abun =  zeros(Int64, size(epi.abundances.matrix, 1), size(epi.abundances.matrix, 2), lensim)
-@test_nowarn simulate!(epi, burnin, timestep)
-@test_nowarn simulate_record!(abun, epi, times, interval, timestep)
-@test all(abun .>= 0)
-@test all(sum(abun, dims = (1,2)) .> 0)
+    # Run simulations 10 times
+    abun =  zeros(Int64, size(epi.abundances.matrix, 1), size(epi.abundances.matrix, 2), lensim)
+    @test_nowarn simulate!(epi, burnin, timestep; save=true, save_path="TEMP")
+    # Check if everything was saved
+    @test isdir("TEMP")
+    @test isfile(joinpath("TEMP", "configuration.jlso"))
+    @test isfile(joinpath("TEMP", "initial_system.jlso"))
+    @test isfile(joinpath("TEMP", "final_system.jlso"))
+    # Delete temporary directory
+    rm("TEMP", recursive=true)
+
+    @test_nowarn simulate_record!(abun, epi, times, interval, timestep; save=true, save_path="TEMP")
+    # Check if everything was saved
+    @test isdir("TEMP")
+    @test isfile(joinpath("TEMP", "configuration.jlso"))
+    @test isfile(joinpath("TEMP", "initial_system.jlso"))
+    @test isfile(joinpath("TEMP", "final_system.jlso"))
+    @test isfile(joinpath("TEMP", "abundances.jlso"))
+    # Delete temporary directory
+    rm("TEMP", recursive=true)
+    @test all(abun .>= 0)
+    @test all(sum(abun, dims = (1,2)) .> 0)
+end
