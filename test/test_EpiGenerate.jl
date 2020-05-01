ea02950988edf988a38518ca7ef0c14940c6aa40
Cook, James <james.cook@ukaea.uk>
2020-05-01 10:44:10 +0100

    comment humanmove test, add TODO
    SCRC-60

--- test/test_EpiGenerate.jl
+++ test/test_EpiGenerate.jl
@@ -12,7 +12,7 @@ totalpops = sum(epi.abundances.matrix, dims = 2)
 @test_nowarn update!(epi, 1day)
 update!(epi, 1day)
 @test all(epi.abundances.matrix .>= 0)
-@test_nowarn humanmove!(epi, 1day)
+#@test_nowarn humanmove!(epi, 1day) # TODO #65
 @test_nowarn virusupdate!(epi, 1day)
 @test_nowarn classupdate!(epi, 1day)
 invalidatecaches!(epi)
