d4a2a4ff906da5565289660a0cc0b7949e9d0874
Cook, James <james.cook@ukaea.uk>
2020-05-05 14:18:27 +0100

    fixes to travis.yml and InstallMPI.sh
    SCRC-87

--- ci/travis/installMPI.sh
+++ ci/travis/installMPI.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # https://www.aithercfd.com/2016/12/03/using-travisci.html
 # Updated to use openMPI 4.0
+mkdir -p ~/openmpi
 cd ~/openmpi
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
 	echo $PWD
@@ -17,7 +18,9 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then
 	ln -s /usr/local/lib lib
 	ln -s /usr/local/include include
 else
+	echo "ls ~/openmpi"
 	ls ~/openmpi
+	echo "ls ~/openmpi-4.0.3"
 	ls ~/openmpi-4.0.3
 	if [ -f "./bin/mpirun" ] && [ -f "$HOME/openmpi-4.0.3/config.log" ]; then
 		echo "Using cached OpenMPI on " $TRAVIS_OS_NAME
@@ -26,16 +29,16 @@ else
 		./configure --prefix=~/openmpi CC=$C_COMPILER CXX=$CXX_COMPILER &> openmpi.configure
 	else
 		echo "Downloading OpenMPI Source on " $TRAVIS_OS_NAME
+		rm -rf ~/openmpi
+		rm -rf ~/openmpi-4.0.3
 		wget https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-4.0.3.tar.gz
 		tar zxf openmpi-4.0.3.tar.gz
 		echo "Configuring and building OpenMPI"
 		mv openmpi-4.0.3 ~
 		cd ~/openmpi-4.0.3
-		./configure --prefix=~/openmpi CC=$C_COMPILER CXX=$CXX_COMPILER &> openmpi.configure
+		./configure --prefix=$HOME/openmpi CC=$C_COMPILER CXX=$CXX_COMPILER &> openmpi.configure
 		make -j &> openmpi.make
 		make install &> openmpi.install
-		echo "PWD = " $PWD
-		ls
 	fi
 	test -n $CC && unset CC
 	test -n $CXX && unset CXX
