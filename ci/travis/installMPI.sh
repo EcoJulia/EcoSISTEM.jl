9b1590f3e260e6f98b5f81d61f6e614fcc253d85
James Cook <cookjws@gmail.com>
2020-05-05 08:59:47 +0100

    InstallMPI.sh openmpi cached to wrong place
    SCRC-87 
    
    See failure https://travis-ci.org/github/ScottishCovidResponse/Simulation.jl/jobs/683003127
    Julia tries to "Activating new environment at `~/build/ScottishCovidResponse/Simulation.jl/openmpi/Project.toml`" - openmpi is in the wrong place for caching

--- ci/travis/installMPI.sh
+++ ci/travis/installMPI.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # https://www.aithercfd.com/2016/12/03/using-travisci.html
 # Updated to use openMPI 4.0
-
+cd ~
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
     cd openmpi
     if [ -f "bin/mpirun" ]; then
@@ -18,19 +18,19 @@ else
 			echo "Using cached OpenMPI"
 			echo "Configuring OpenMPI"
 			cd openmpi-4.0.3
-			./configure --prefix=$TRAVIS_BUILD_DIR/openmpi CC=$C_COMPILER CXX=$CXX_COMPILER &> openmpi.configure
+			./configure --prefix=~/openmpi CC=$C_COMPILER CXX=$CXX_COMPILER &> openmpi.configure
 		else
 			echo "Downloading OpenMPI Source"
 			wget https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-4.0.3.tar.gz
 			tar zxf openmpi-4.0.3.tar.gz
 			echo "Configuring and building OpenMPI"
 			cd openmpi-4.0.3
-			./configure --prefix=$TRAVIS_BUILD_DIR/openmpi CC=$C_COMPILER CXX=$CXX_COMPILER &> openmpi.configure
+			./configure --prefix=$PWD/openmpi CC=$C_COMPILER CXX=$CXX_COMPILER &> openmpi.configure
 			make -j4 &> openmpi.make
 			make install &> openmpi.install
-			cd ..
-			export JULIA_MPI_PATH=$TRAVIS_BUILD_DIR/openmpi
+			export JULIA_MPI_PATH=$PWD/openmpi
     fi
     test -n $CC && unset CC
     test -n $CXX && unset CXX
 fi
+cd $TRAVIS_BUILD_DIR
