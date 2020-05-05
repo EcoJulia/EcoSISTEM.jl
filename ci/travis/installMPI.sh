505cf460ba94ddacf3dfa00b18300a204db6bcf5
Cook, James <james.cook@ukaea.uk>
2020-05-05 15:05:04 +0100

    old cache messing things up travis.yml and InstallMPI.sh
    SCRC-87

--- ci/travis/installMPI.sh
+++ ci/travis/installMPI.sh
@@ -1,35 +1,28 @@
 #!/bin/bash
 # https://www.aithercfd.com/2016/12/03/using-travisci.html
 # Updated to use openMPI 4.0
-mkdir -p ~/openmpi
-cd ~/openmpi
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-	echo $PWD
-	ls *
 	if [ -f "./bin/mpirun" ]; then
 		echo "Using cached OpenMPI on " $TRAVIS_OS_NAME
     else
 		echo "Installing OpenMPI with homebrew on " $TRAVIS_OS_NAME
 		rm -rf ~/openmpi
 		rm -rf ~/openmpi-4.0.3
+		mkdir -p ~/openmpi
+		cd ~/openmpi
 		NUM_CORES=$(sysctl -n hw.ncpu)
 		HOMEBREW_MAKE_JOBS=$NUM_CORES brew install open-mpi
-		cp -r /usr/local/Cellar/open-mpi/4.0.3/* .
+		cp -r /usr/local/Cellar/open-mpi/4.0.3/* . # 4.0.3 will be out of date eventually
     fi
 	ln -s /usr/local/bin bin
 	ln -s /usr/local/lib lib
 	ln -s /usr/local/include include
 else
-	echo "ls ~/openmpi"
-	ls ~/openmpi
-	echo "ls ~/openmpi-4.0.3"
-	ls ~/openmpi-4.0.3
-	if [ -f "./bin/mpirun" ] && [ -f "$HOME/openmpi-4.0.3/config.log" ]; then
+	cd ~/openmpi
+	if [ -f "$HOME/bin/mpirun" ] && [ -f "$HOME/openmpi-4.0.3/config.log" ]; then
 		echo "Using cached OpenMPI on " $TRAVIS_OS_NAME
 		echo "Configuring OpenMPI"
-		rm -rf ~/openmpi
-		rm -rf ~/openmpi-4.0.3
-		cd ../openmpi-4.0.3
+		cd ~/openmpi-4.0.3
 		./configure --prefix=~/openmpi CC=$C_COMPILER CXX=$CXX_COMPILER &> openmpi.configure
 	else
 		echo "Downloading OpenMPI Source on " $TRAVIS_OS_NAME
