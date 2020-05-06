5277ac24920877b69258d57f235c01c73119961f
Sean Lovett <sean.lovett@invenialabs.co.uk>
2020-05-06 17:26:29 +0100

    Test constructor from initial population

--- test/test_EpiEnv.jl
+++ test/test_EpiEnv.jl
@@ -20,3 +20,23 @@ abenv = simplehabitatAE(fillval, grid, area, control)
 @test abenv.active == active
 @test all(abenv.active)
 @test abenv.habitat.size^2 * grid[1] * grid[2] == area
+
+@testset "Constructing from initial population" begin
+    fillval = 1.0
+    area = 25.0km^2
+    control = NoControl()
+    initial_pop = rand(10, 15) * 10
+    # Fill some NaNs, these should indicate inactive areas
+    missing_idx = CartesianIndex.([1, 4, 8], [5, 8, 2])
+    initial_pop[missing_idx] .= NaN
+
+    expected_active = Matrix{Bool}(.!isnan.(initial_pop))
+    expected_size = size(initial_pop)
+    expected_pop = round.(replace(initial_pop, NaN => 0))
+
+    epienv = simplehabitatAE(fillval, area, control, initial_pop)
+    @test all(epienv.habitat.matrix .== fillval)
+    @test size(epienv.habitat.matrix) == expected_size
+    @test epienv.active == expected_active
+    @test epienv.initial_population == expected_pop
+end
