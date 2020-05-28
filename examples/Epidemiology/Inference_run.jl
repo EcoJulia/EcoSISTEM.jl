2b37df1be6fce7bdc1a87f8b4a20132bdac38856
Claire Harris <claire.harris@bioss.ac.uk>
2020-05-28 14:52:25 +0100

    Updates to inference wrappers. Refers to ScottishCovidResponse/SCRCIssueTracking#310 Remove unnecessary set up and create allocating/non-allocating versions of wrappers.

--- examples/Epidemiology/Inference_run.jl
+++ examples/Epidemiology/Inference_run.jl
@@ -11,9 +11,13 @@ using Unitful.DefaultSymbols
 # virus_decay = decay rate of environmental reservoir
 # mean_dispersal_dist = average dispersal distance of virus per each infected
 param = (beta_env = 1.0/day, beta_force = 1.0/day, sigma = 0.02/day, virus_growth = 1e-3/day, virus_decay = 1e-3/day, mean_dispersal_dist = 10.0km)
+# Set run parameters:
+# times = amount of time to simulate over
+# interval = how often to record output
+# timestep = timestep of simulation
+runparams = (times = 2years, interval = 1day, timestep = 1day)
 # Set grid size & area
 grid_size = (4,4)
 area = 100.0km^2
-repeats = 10
-abuns = SIR_wrapper(grid_size, area, param, repeats)
-# This set up runs in ~0.14 seconds per repeat.
+@time abuns = SIR_wrapper(grid_size, area, param, runparams)
+# This set up runs in ~0.2 seconds per repeat.
--- src/Epidemiology/Inference.jl
+++ src/Epidemiology/Inference.jl
@@ -1,65 +1,72 @@
 """
-    SIR_wrapper(grid_size::Tuple{Int64, Int64}, reps::Int64=1)
+    SIR_wrapper(grid_size::Tuple{Int64, Int64}, area::Unitful.Area{Float64}, params::NamedTuple, runtimes::NamedTuple)
 
-Function to simulate simple SIR stochastic realisations, given a grid dimension, `grid_size`, area size, set of parameters, `params` and number of repeats, `reps`.
+Function to simulate simple SIR stochastic realisations, given a grid dimension, `grid_size`, area size, a set of model parameters, `params`, and running parameters, `runtimes`.
 
-Outputs an vector of abundance matrices, one for each repeat, of compartment by grid cell. Compartments for the SIR model are: Susceptible, Infected, Recovered, Dead.
+Outputs an abundance matrice of compartment by grid cell over time. Compartments for the SIR model are: Susceptible, Infected, Recovered, Dead.
 """
-function SIR_wrapper(grid_size::Tuple{Int64, Int64}, area::Unitful.Area{Float64}, params::NamedTuple, reps::Int64=1)
+function SIR_wrapper(grid_size::Tuple{Int64, Int64}, area::Unitful.Area{Float64}, params::NamedTuple, runtimes::NamedTuple)
+    times = runtimes.times; interval = runtimes.interval
+    Ncells = grid_size[1] * grid_size[2]
+    numclasses = 4
+    abuns = zeros(Int64, numclasses + 1, Ncells, convert(Int64, floor(times / interval)) + 1)
+    return SIR_wrapper!(grid_size, area, params, runtimes, abuns)
+end
+"""
+    SIR_wrapper!(grid_size::Tuple{Int64, Int64}, area::Unitful.Area{Float64}, params::NamedTuple, runtimes::NamedTuple, abuns::Array{Int64, 3})
+
+Function to simulate simple SIR stochastic realisations, given a grid dimension, `grid_size`, area size, a set of model parameters, `params`, and running parameters, `runtimes`.
+
+Fills an abundance matrice of compartment by grid cell over time. Compartments for the SIR model are: Susceptible, Infected, Recovered, Dead.
+"""
+function SIR_wrapper!(grid_size::Tuple{Int64, Int64}, area::Unitful.Area{Float64}, params::NamedTuple, runtimes::NamedTuple, abuns::Array{Int64, 3})
     # Set up
-    do_save = (@isdefined do_save) ? do_save : false
-    save_path = (@isdefined save_path) ? save_path : pwd()
-    abuns = Vector{Array{Int64, 3}}(undef, reps)
     numclasses = 4
     Ncells = grid_size[1] * grid_size[2]
 
     # Extract model params from tuple
-    beta_force = params[:beta_force]
-    beta_env = params[:beta_env]
-    sigma = params[:sigma]
-    virus_growth = params[:virus_growth]
-    virus_decay = params[:virus_decay]
-    mean_dispersal_dist = params[:mean_dispersal_dist]
-    # Loop through repeats
-    for i in 1:reps
-        # Set simulation parameters & create transition matrices
-        birth = fill(0.0/day, numclasses)
-        death = fill(0.0/day, numclasses)
-        param = SIRGrowth{typeof(unit(beta_force))}(birth, death, virus_growth, virus_decay, beta_force, beta_env, sigma)
-        param = transition(param)
+    beta_force = params.beta_force
+    beta_env = params.beta_env
+    sigma = params.sigma
+    virus_growth = params.virus_growth
+    virus_decay = params.virus_decay
+    mean_dispersal_dist = params.mean_dispersal_dist
 
-        # Set up simple gridded environment
-        epienv = simplehabitatAE(298.0K, grid_size, area, NoControl())
+    # Set simulation parameters & create transition matrices
+    birth = fill(0.0/day, numclasses)
+    death = fill(0.0/day, numclasses)
+    param = SIRGrowth{typeof(unit(beta_force))}(birth, death, virus_growth, virus_decay, beta_force, beta_env, sigma)
+    param = transition(param)
 
-        # Set initial population sizes for all categories: Virus, Susceptible, Infected, Recovered
-        initial_pops = (
-            virus = 0,
-            susceptible = 500_000 * Ncells,
-            infected = 100 * Ncells,
-            recovered = 0,
-            dead = 0,
-        )
-        abun = [initial_pops...]
+    # Set up simple gridded environment
+    epienv = simplehabitatAE(298.0K, grid_size, area, NoControl())
 
-        # Dispersal kernels for virus and disease classes
-        dispersal_dists = fill(sqrt(area/Ncells)/5, numclasses)
-        dispersal_dists[2] = mean_dispersal_dist
-        kernel = GaussianKernel.(dispersal_dists, 1e-10)
-        movement = AlwaysMovement(kernel)
+    # Set initial population sizes for all categories: Virus, Susceptible, Infected, Recovered
+    initial_pops = (
+        virus = 0,
+        susceptible = 500_000 * Ncells,
+        infected = 100 * Ncells,
+        recovered = 0,
+        dead = 0,
+    )
+    abun = [initial_pops...]
 
-        # Traits for match to environment (turned off currently through param choice, i.e. virus matches environment perfectly)
-        traits = GaussTrait(fill(298.0K, numclasses), fill(0.1K, numclasses))
-        epilist = SIR(traits, abun, movement, param)
+    # Dispersal kernels for virus and disease classes
+    dispersal_dists = fill(sqrt(area/Ncells)/5, numclasses)
+    dispersal_dists[2] = mean_dispersal_dist
+    kernel = GaussianKernel.(dispersal_dists, 1e-10)
+    movement = AlwaysMovement(kernel)
 
-        # Create epi system with all information
-        rel = Gauss{eltype(epienv.habitat)}()
-        epi = EpiSystem(epilist, epienv, rel)
+    # Traits for match to environment (turned off currently through param choice, i.e. virus matches environment perfectly)
+    traits = GaussTrait(fill(298.0K, numclasses), fill(0.1K, numclasses))
+    epilist = SIR(traits, abun, movement, param)
 
-        # Run simulation
-        times = 2years; interval = 1day; timestep = 1day
-        abuns[i] = zeros(Int64, size(epi.abundances.matrix, 1), Ncells, convert(Int64, floor(times / interval)) + 1)
-        thisabun = abuns[i]
-        @time simulate_record!(thisabun, epi, times, interval, timestep;save=do_save, save_path=joinpath(save_path, "rep_$(i)"))
-    end
+    # Create epi system with all information
+    rel = Gauss{eltype(epienv.habitat)}()
+    epi = EpiSystem(epilist, epienv, rel)
+
+    # Run simulation
+    times = runtimes.times; interval = runtimes.interval; timestep = runtimes.timestep
+    simulate_record!(abuns, epi, times, interval, timestep; save=false)
     return abuns
 end
