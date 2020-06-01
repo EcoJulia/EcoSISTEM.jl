2d570d1ef2eb638743b5d3b6eff566178f558f4b
Claire Harris <claire.harris@bioss.ac.uk>
2020-06-01 17:01:14 +0100

    Consolidate EpiList constructor functions into a single call, which creates an epilist from NamedTuples. Refers to ScottishCovidResponse/SCRCIssueTracking#328

--- src/Epidemiology/EpiList.jl
+++ src/Epidemiology/EpiList.jl
@@ -98,105 +98,23 @@ function _getdiversityname(el::EpiList)
 end
 
 """
-    SIS(traits::TR, virus_abun::Vector{Int64}, human_abun::Vector{Int64}, movement::MO, params::P, age_categories::Int64 = 1) where {TR <: AbstractTraits, MO <: AbstractMovement, P <: AbstractParams}
+    EpiList(traits::TR, virus_abun::Vector{Int64}, human_abun::Vector{Int64}, movement::MO, params::P, age_categories::Int64 = 1) where {TR <: AbstractTraits, MO <: AbstractMovement, P <: AbstractParams}
 
-Function to create an `EpiList` for an SIS model - creating the correct number of classes and checking dimensions.
+Function to create an `EpiList` for any type of epidemiological model - creating the correct number of classes and checking dimensions.
 """
-function SIS(traits::TR, virus_abun::Vector{Int64}, human_abun::Vector{Int64}, movement::MO, params::P, age_categories::Int64 = 1) where {TR <: AbstractTraits,
-        MO <: AbstractMovement, P <: AbstractParams}
-
-    names = ["Susceptible", "Infected", "Dead"]
+function EpiList(traits::TR, virus_abun::NamedTuple, human_abun::NamedTuple, movement::MO, params::P, age_categories::Int64 = 1) where {TR <: AbstractTraits, MO <: AbstractMovement, P <: AbstractParams}
+    names = collect(string.(keys(human_abun)))
     new_names = [ifelse(i == 1, "$j", "$j$i") for i in 1:age_categories, j in names][1:end]
     ht = UniqueTypes(length(new_names))
-    human = HumanTypes{typeof(movement), typeof(ht)}(new_names, human_abun, ht, movement)
+    human = HumanTypes{typeof(movement), typeof(ht)}(new_names, vcat(collect(human_abun)...), ht, movement)
 
-    virus_names = [ifelse(i == 1, "Virus", "Virus$i") for i in 1:length(virus_abun)]
+    virus_names = collect(string.(keys(virus_abun)))
+    virus_names = [ifelse(i == 1, "$(virus_names[i])", "$(virus_names[i])$i") for i in 1:length(virus_abun)]
     vt = UniqueTypes(length(virus_names))
-    virus = VirusTypes{typeof(traits), typeof(vt)}(virus_names, traits, virus_abun, vt)
+    virus = VirusTypes{typeof(traits), typeof(vt)}(virus_names, traits, vcat(collect(virus_abun)...), vt)
 
     length(movement.kernels) == length(new_names) || throw(DimensionMismatch("Movement vector doesn't match number of disease classes"))
     length(traits.mean) == length(virus_names) || throw(DimensionMismatch("Trait vector doesn't match number of virus classes"))
     size(params.transition, 1) == length(new_names) || throw(DimensionMismatch("Transition matrix doesn't match number of disease classes"))
-  EpiList{typeof(params)}(virus, human, params)
-end
-
-"""
-    SIR(traits::TR, virus_abun::Vector{Int64}, human_abun::Vector{Int64}, movement::MO, params::P, age_categories::Int64 = 1) where {TR <: AbstractTraits, MO <: AbstractMovement, P <: AbstractParams}
-
-Function to create an `EpiList` for an SIR model - creating the correct number of classes and checking dimensions.
-"""
-function SIR(traits::TR, virus_abun::Vector{Int64}, human_abun::Vector{Int64}, movement::MO, params::P, age_categories::Int64 = 1) where {TR <: AbstractTraits,
-        MO <: AbstractMovement, P <: AbstractParams}
-
-    names = ["Susceptible", "Infected", "Recovered", "Dead"]
-    new_names = [ifelse(i == 1, "$j", "$j$i") for i in 1:age_categories, j in names][1:end]
-    ht = UniqueTypes(length(new_names))
-    human = HumanTypes{typeof(movement), typeof(ht)}(new_names, human_abun, ht, movement)
-
-    virus_names = [ifelse(i == 1, "Virus", "Virus$i") for i in 1:length(virus_abun)]
-    vt = UniqueTypes(length(virus_names))
-    virus = VirusTypes{typeof(traits), typeof(vt)}(virus_names, traits, virus_abun, vt)
-
-    length(movement.kernels) == length(new_names) || throw(DimensionMismatch("Movement vector doesn't match number of disease classes"))
-    length(traits.mean) == length(virus_names) || throw(DimensionMismatch("Trait vector doesn't match number of virus classes"))
-    size(params.transition, 1) == length(new_names) || throw(DimensionMismatch("Transition matrix doesn't match number of disease classes"))
-  EpiList{typeof(params)}(virus, human, params)
-end
-
-"""
-    SEIR(traits::TR, virus_abun::Vector{Int64}, human_abun::Vector{Int64}, movement::MO, params::P, age_categories::Int64 = 1) where {TR <: AbstractTraits, MO <: AbstractMovement, P <: AbstractParams}
-
-Function to create an `EpiList` for an SEIR model - creating the correct number of classes and checking dimensions.
-"""
-function SEIR(traits::TR, virus_abun::Vector{Int64}, human_abun::Vector{Int64}, movement::MO, params::P, age_categories::Int64 = 1) where {TR <: AbstractTraits,
-        MO <: AbstractMovement, P <: AbstractParams}
-
-    names = ["Susceptible", "Exposed", "Infected", "Recovered", "Dead"]
-    new_names = [ifelse(i == 1, "$j", "$j$i") for i in 1:age_categories, j in names][1:end]
-    ht = UniqueTypes(length(new_names))
-    human = HumanTypes{typeof(movement), typeof(ht)}(new_names, human_abun, ht, movement)
-
-    virus_names = [ifelse(i == 1, "Virus", "Virus$i") for i in 1:length(virus_abun)]
-    vt = UniqueTypes(length(virus_names))
-    virus = VirusTypes{typeof(traits), typeof(vt)}(virus_names, traits, virus_abun, vt)
-
-    length(movement.kernels) == length(new_names) || throw(DimensionMismatch("Movement vector doesn't match number of disease classes"))
-    length(traits.mean) == length(virus_names) || throw(DimensionMismatch("Trait vector doesn't match number of virus classes"))
-    size(params.transition, 1) == length(new_names) || throw(DimensionMismatch("Transition matrix doesn't match number of disease classes"))
-  EpiList{typeof(params)}(virus, human, params)
-end
-
-"""
-    SEIRS(traits::TR, virus_abun::Vector{Int64}, human_abun::Vector{Int64}, movement::MO, params::P, age_categories::Int64 = 1) where {TR <: AbstractTraits, MO <: AbstractMovement, P <: AbstractParams}
-
-Function to create an `EpiList` for an SEIRS model - creating the correct number of classes and checking dimensions.
-"""
-function SEIRS(traits::TR, virus_abun::Vector{Int64}, human_abun::Vector{Int64}, movement::MO, params::P, age_categories::Int64 = 1) where {TR <: AbstractTraits,
-        MO <: AbstractMovement, P <: AbstractParams}
-
-    return SEIR(traits, virus_abun, human_abun, movement, params, age_categories)
-end
-
-"""
-    SEI2HRD(traits::TR, virus_abun::Vector{Int64}, human_abun::Vector{Int64}, movement::MO, params::P, age_categories::Int64 = 1) where {TR <: AbstractTraits, MO <: AbstractMovement, P <: AbstractParams}
-
-Function to create an `EpiList` for an SEI2HRD model - creating the correct number of classes and checking dimensions.
-"""
-function SEI2HRD(traits::TR, virus_abun::Vector{Int64}, human_abun::Vector{Int64}, movement::MO, params::P, age_categories::Int64 = 1) where {TR <: AbstractTraits,
-        MO <: AbstractMovement, P <: AbstractParams}
-
-    names = ["Susceptible", "Exposed", "AsymptomaticInfected", "SymptomaticInfected", "Hospitalised", "Recovered", "Dead"]
-    new_names = [ifelse(i == 1, "$j", "$j$i") for i in 1:age_categories, j in names][1:end]
-    ht = UniqueTypes(length(new_names))
-    human = HumanTypes{typeof(movement), typeof(ht)}(new_names, human_abun, ht, movement)
-
-    virus_names = [ifelse(i == 1, "Virus", "Virus$i") for i in 1:length(virus_abun)]
-    vt = UniqueTypes(length(virus_names))
-    virus = VirusTypes{typeof(traits), typeof(vt)}(virus_names, traits, virus_abun, vt)
-
-    length(movement.kernels) == length(new_names) || throw(DimensionMismatch("Movement vector doesn't match number of disease classes"))
-    length(traits.mean) == length(virus_names) || throw(DimensionMismatch("Trait vector doesn't match number of virus classes"))
-    size(params.transition, 1) == length(new_names) || throw(DimensionMismatch("Transition matrix doesn't match number of disease classes"))
-    length(params.births) == length(new_names) || throw(DimensionMismatch("Birth vector doesn't match number of disease classes"))
-  EpiList{typeof(params)}(virus, human, params)
+    return EpiList{typeof(params)}(virus, human, params)
 end
