5bd2b409b76aa0ec4c15b62fd762fdb2cf58c8ec
Bella Wu <bella.wu@invenialabs.co.uk>
2020-06-01 14:43:06 +0100

    initial functions to save abuns to h5

--- src/Epidemiology/EpiHelper.jl
+++ src/Epidemiology/EpiHelper.jl
@@ -101,3 +101,84 @@ function simulate_record!(
   save && JLSO.save(joinpath(save_path, "abundances.jlso"), :abundances => storage)
   return storage
 end
+
+function initialise_output_abuns(
+    abuns::Array,
+    axes::NamedTuple;
+    h5fn=joinpath(pwd(),"abundances.h5")
+)
+    # - sanity check
+    if size(abuns) != length.(values(axes))
+        throw(DimensionMismatch(
+            "abundances array size $(size(abuns)) does not match axes size ", *
+            "axes: $(keys(axes)). vector length: $(length.(values(axes)))"
+        ))
+    end
+
+    # - initialise HDF5 file
+    h5open(h5fn, "w") do fid
+        # - create group
+        group = g_create(fid, "abundances")
+        attrs(group)["Description"] = String(
+            "Contains the abundances for each compartment and geographic location ",
+            "through the simulation duration."
+        )
+        # - add data to the group
+        # initialise abuns 3D array
+        dset_abuns = d_create(
+            group,
+            "abuns",
+            datatype(Int),
+            dataspace(size(abuns)),
+            "chunk",
+            (size.(Ref(abuns), [1,2])...,1)
+        )
+        # fill in axes information
+        for k in keys(axes)
+            group[string(k)] = axes[k]
+        end
+    end
+end
+
+function update_output_abuns(
+    abuns::Array,
+    axes::NamedTuple,
+    timestep::Int;
+    h5fn=joinpath(pwd(),"abundances.h5")
+)
+    h5open(h5fn, "cw") do fid
+        fid["abundances"]["abuns"][:,:,timestep] = epi.abundances.matrix
+    end
+end
+
+
+
+# TEMP
+using AxisArrays
+using HDF5
+
+
+grid_id = map(Iterators.product(axisvalues(epi.epienv.initial_population)...)) do (x,y)
+    return string.(Int(x), "-", Int(y))
+end
+grid_id = vec(grid_id) # TODO: construct `grid-id` correctly
+
+
+axes = (;
+    compartment = epi.epilist.human.names,
+    grid_id = grid_id,
+    times = string.(0s:interval:times) # TODO, use `record_seq`, better to convert `s` to `day`
+)
+
+# before iteration
+
+
+# for each iteration
+fid = h5open("test.h5", "cw")
+fid["abundances"]["abuns"][:,:,1] = epi.abundances.matrix # TODO 1 to `counting`
+close(fid)
+
+# - to check what's saved
+f = h5open("test.h5", "r")
+abuns_data = read(f["abundances"]["abuns"])
+close(f)
