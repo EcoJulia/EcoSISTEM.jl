6b93dc2c5388b6e6e28f5a3c4d8bfa4a33a15088
Eric Perim <eric.perim@invenialabs.co.uk>
2020-06-03 21:58:36 +0100

    Fix grid parsing

--- src/Epidemiology/data_utils.jl
+++ src/Epidemiology/data_utils.jl
@@ -4,7 +4,7 @@ using HDF5
     parse_scotpop(path; grid="10k")
 
 Parse HDF5-format file at `path`, containing the Scottish population by age. The data is
-read on a `grid`x`grid` square grid.
+read on a grid with cells of size `grid` meters x `grid` meters.
 """
 function parse_scotpop(path; grid="10k")
     # TODO write a method that automatically downloads the file from Github if no path is
@@ -19,20 +19,27 @@ function parse_scotpop(path; grid="10k")
     ages = [string(5*i)*"-"*string(5*i+4) for i in 0:17]
     push!(ages, "90+")
 
+    # Get rows and columns
+    field = "grid" * grid
+    cell_ids = map(x -> parse.(Int, split(x, "-")), data[field*"_id"])
+    max_row = maximum(getindex.(cell_ids, 1))
+    max_column = maximum(getindex.(cell_ids, 2))
+
     # Create empty grid
-    grid_size = parse(Int, chop(grid)) * 1000
+    cell_size = parse(Int, chop(grid))km
     population = AxisArray(
-        zeros(grid_size, grid_size, length(ages)),
-        grid_x=collect(1:grid_size),
-        grid_y=collect(1:grid_size),
+        zeros(max_row, max_column, length(ages)),
+        # Placing the coordinates of the cell as the distance between its origin and the
+        # origin of the grid.
+        grid_x=[cell_size * (i - 1) for i in 1:max_row],
+        grid_y=[cell_size * (i - 1) for i in 1:max_column],
         age=ages,
     )
 
     # Populate grid
     field = "grid" * grid
-    for (id, pop) in zip(data[field*"_id"], eachrow(data[field*"_binned"]))
-        x, y = parse.(Int, split(id, "-"))
-        population[grid_x = x, grid_y = y] = pop
+    for (id, pop) in zip(cell_ids, eachrow(data[field*"_binned"]))
+        population[grid_x = id[1], grid_y = id[2]] = pop
     end
 
     return population
--- test/test_data_utils.jl
+++ test/test_data_utils.jl
@@ -1,9 +1,9 @@
 @testset "Data Utils" begin
     path = Simulation.path("test", "examples", "scrc_demographics.h5")
     scotpop_10k = parse_scotpop(path)
-    @test size(scotpop_10k) == (10000, 10000, 19)
+    @test size(scotpop_10k) == (70, 47, 19)
     scotpop_1k = parse_scotpop(path, grid="1k")
-    @test size(scotpop_1k) == (1000, 1000, 19)
+    @test size(scotpop_1k) == (691, 465, 19)
     @test sum(scotpop_1k) == sum(scotpop_10k)
     @test_throws ArgumentError parse_scotpop(path, grid="5k")
 end
