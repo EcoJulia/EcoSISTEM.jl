26b3b7d1447613d6a5e6d01a7bc857d57dd856f6
Claire Harris <claire.harris@bioss.ac.uk>
2020-06-10 16:10:55 +0100

    Update load examples. Refers to ScottishCovidResponse/SCRCIssueTracking#11

--- examples/Epidemiology/loadMetData.jl
+++ examples/Epidemiology/loadMetData.jl
@@ -1,21 +1,35 @@
-using Simulation.ClimatePref
-using Unitful.DefaultSymbols
+using Simulation
 using Simulation.Units
-
-dir = "test/examples/dailyrain/"
-file = "ukv"
-dim = collect(1day:1day:3days)
-param = "stratiform_rainfall_flux"
-ukrain = readMet(dir, file, param, dim)
-
-using Plots
+using Simulation.ClimatePref
+using Dates
 using Unitful
-display(heatmap(ustrip.(ukrain[:, :, 1]')))
+using HTTP
+using AxisArrays
+using Unitful.DefaultSymbols
 
-dir = "test/examples/temp/"
-file = "ukv"
-dim = collect(1day:1day:3days)
-param = "air_temperature"
-uktemp = readMet(dir, file, param, dim)
 
-display(heatmap(ustrip.(uktemp[:, :, 1]')))
+# Show available data
+getMetdata()
+getMetparams()
+
+# Test with different variables
+startDate = DateTime("2020-01-01")
+endDate = DateTime("2020-01-02")
+outputfolder = "test/examples/rain"
+ukrain = MetOfficeDownload(:uk_daily, :rain_mean, outputfolder, startDate, endDate)
+
+outputfolder = "test/examples/humidity"
+ukhumid = MetOfficeDownload(:uk_daily, :humidity_mean, outputfolder, startDate, endDate)
+
+outputfolder = "test/examples/sunshine"
+uksun = MetOfficeDownload(:uk_daily, :sunshine_mean, outputfolder, startDate, endDate)
+
+outputfolder = "test/examples/temp"
+uktemp = MetOfficeDownload(:uk_daily, :temp_mean, outputfolder, startDate, endDate)
+
+# Set up simple gridded environment
+area = 501_768.0km^2
+grid = (621, 808)
+active = fill(true, grid)
+active[1:10, :] .= false
+epienv = ukclimateAE(ukrain, grid, area, active, NoControl())
