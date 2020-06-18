de4c073dd0885aec128a98cd56a12a41d8b12c36
Claire Harris <claire.harris@bioss.ac.uk>
2020-06-18 11:22:57 +0100

    Function to process Met Office data into same map projection as Scottish pop data (BNG). Refers to ScottishCovidResponse/SCRCIssueTracking#11

--- src/ClimatePref/ReadData.jl
+++ src/ClimatePref/ReadData.jl
@@ -4,6 +4,7 @@ using Simulation.Units
 using AxisArrays
 using NetCDF
 using Compat
+using RCall
 
 import Unitful.°, Unitful.°C, Unitful.mm, Unitful.hr
 import ArchGDAL
@@ -52,6 +53,40 @@ function readMet(dir::String, param::String)
     return uk
 end
 
+function processMet(dir::String, param::String, xmin::Float64 = 5513, xmax::Float64 = 470513, ymin::Float64 = 530301.5, ymax::Float64 = 1220302, res::Int64 = 1000)
+    # Alter projection of netcdf data and resample to BNG
+    @rput xmin xmax ymin ymax res
+    @rput dir
+    R"library(raster);
+    tmpin = raster(dir)
+    crs(tmpin) = '+proj=ob_tran +o_proj=longlat +lon_0=357.5 +o_lon_p=0 +o_lat_p=37.5 +a=6371229 +b=6371229 +to_meter=0.0174532925199 +wktext'
+    extent(tmpin)[1] = extent(tmpin)[1] - 360
+    extent(tmpin)[2] = extent(tmpin)[2] - 360
+    ext = extent(xmin, xmax, ymin, ymax)
+    proj = '+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +towgs84=446.448,-125.157,542.06,0.15,0.247,0.842,-20.489 +units=m +no_defs'
+    resol = c(res, res)
+    r = raster(ext = ext, resolution = resol, crs = proj)
+    reproj = projectRaster(from = tmpin, crs = proj)
+    reproj = resample(reproj, r)
+    newdat = reproj@data@values
+    x = reproj@nrows
+    y = reproj@ncols
+    "
+    @rget newdat
+    @rget x y
+    # Reshape output and add units
+    xs = (xmin+res/2:res:xmax)  .* m
+    ys = (ymin:res:ymax) .* m
+    reshaped_dat = reshape(newdat', y, x)
+    reshaped_dat = reshaped_dat[:, end:-1:1]
+    reshaped_dat = hcat(reshaped_dat, fill(missing, y))
+    reshaped_dat = replace(reshaped_dat, missing=>NaN)
+    units = ncgetatt(dir, param, "units")
+    units = unitdict[units]
+    uk = AxisArray(reshaped_dat .* units, Axis{:longitude}(xs), Axis{:latitude}(ys))
+    return uk
+end
+
 """
     readMet(dir::String, file::String, param::String, dim::Vector{Vector{T}})
         where T<: Unitful.Time
@@ -59,11 +94,15 @@ end
 Function to extract a certain parameter, `param`, from a directory, `dir`, containing Met Office netcdf files,
 for a certain timerange, `dim`, and convert into an axis array.
 """
-function readMet(dir::String, file::String, param::String, dim::Vector{T}) where T <: Unitful.Time
+function readMet(dir::String, file::String, param::String, dim::Vector{T}, process::Bool = true) where T <: Unitful.Time
     filenames = searchdir(dir, file)
     newmet = Array{AxisArray, 1}(undef, length(filenames))
     for i in eachindex(filenames)
-        newmet[i] = readMet(joinpath(dir, filenames[i]), param)
+        if process
+            newmet[i] = processMet(joinpath(dir, filenames[i]), param)
+        else
+            newmet[i] = readMet(joinpath(dir, filenames[i]), param)
+        end
     end
     catmet = cat(dims=3, newmet ...)
     catmet = AxisArray(catmet, Axis{:longitude}(catmet.axes[1]), Axis{:latitude}(catmet.axes[2]), Axis{:day}(dim))
