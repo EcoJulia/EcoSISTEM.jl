83ab9fa2f62532f401847be7aacfb157b5ca2823
Claire Harris <claire.harris@bioss.ac.uk>
2020-07-14 12:45:00 +0100

    Fix bug in _lookup. Refers to ScottishCovidResponse/SCRCIssueTracking#537

--- src/Epidemiology/EpiSystem.jl
+++ src/Epidemiology/EpiSystem.jl
@@ -233,8 +233,8 @@ end
 
 function genlookups(from::Int64, to::Vector{Int64}, xys::Array{Tuple{Int64,Int64},1}, grid_size::Float64, relsize::Float64, thresh::Float64, epienv::GridEpiEnv)
     x, y = xys[to .== from][1]
-    maxX = ceil(Int64, x + grid_size * 2/relsize); minX = ceil(Int64, x - grid_size * 2/relsize)
-    maxY = floor(Int64, y + grid_size * 2/relsize); minY = floor(Int64, y - grid_size * 2/relsize)
+    maxX = ceil(Int64, x + 1/relsize); minX = ceil(Int64, x - 1/relsize)
+    maxY = floor(Int64, y + 1/relsize); minY = floor(Int64, y - 1/relsize)
     keep = [(i[1] <= maxX) & (i[2] <= maxY) & (i[1] >= minX) & (i[2] >= minY) for i in xys]
     to = to[keep]
     probs = [_lookup((x = x, y = y), (x = i[1], y = i[2]), relsize, _gaussian_disperse) for i in xys[keep]]
@@ -246,7 +246,7 @@ end
 
 function _lookup(from::NamedTuple, to::NamedTuple, relSquareSize::Float64, dispersalfn::F) where {F<:Function}
     return calc_prob = hcubature(r -> dispersalfn(r),
-      [from.x - relSquareSize, from.y - relSquareSize, to.x - relSquareSize, to.y - relSquareSize],
-      [from.x, from.y, to.x, to.y],
+      [from.y *relSquareSize - relSquareSize, from.x * relSquareSize - relSquareSize, to.y * relSquareSize - relSquareSize, to.x * relSquareSize - relSquareSize],
+      [from.y * relSquareSize, from.x * relSquareSize, to.y * relSquareSize, to.x * relSquareSize],
       maxevals= 100, rtol = 0.01)[1] / relSquareSize^2
 end
