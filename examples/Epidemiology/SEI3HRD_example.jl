8632824eac3677e60825a8b40bad2b416cc8fccf
Richard Reeve <git@richardreeve.net>
2020-07-22 22:46:14 +0100

    Fix deterministic median() for Multinomial

--- examples/Epidemiology/SEI3HRD_example.jl
+++ examples/Epidemiology/SEI3HRD_example.jl
@@ -23,10 +23,30 @@ rand(::MedianGenerator, dist::Distribution{Univariate,S}) where {S<:ValueSupport
 rand(::MedianGenerator, dist::Binomial) = median(dist)
 
 # guess how to determine the median from Multivariate
-median(dist::Distribution{Multivariate,S}) where {S<:ValueSupport} = dist.n .* dist.p
+function median(dist::Multinomial)
+    values = dist.n .* dist.p
+    rounded = round.(Int64, values)
+    values .-= rounded
+    order = sortperm(values)
+    count = sum(rounded) - dist.n
+    
+    n = 1
+    while count > 0
+        if rounded[order[n]] > 0
+            rounded[order[n]] -= 1
+            count -= 1
+        end
+        n += 1
+    end
+
+    rounded[order[1:-count]] .+= 1
+
+    return rounded
+end
+
 function rand!(::MedianGenerator, dist::Distribution{Multivariate,S},
         container::AbstractVector{T}) where {S<:ValueSupport, T<:Integer}
-    container .= T.(round.(median(dist)))
+    container .= T.(median(dist))
     return nothing
 end
 
