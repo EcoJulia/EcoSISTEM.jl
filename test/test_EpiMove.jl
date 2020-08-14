60329aa93a01079555dacdb70ca1a003e487b225
Claire Harris <claire.harris@bioss.ac.uk>
2020-08-14 11:38:25 +0100

    Add more tests for EpiMovement

---
+++ test/test_EpiMove.jl
@@ -0,0 +1,22 @@
+using Simulation
+using Test
+using Unitful.DefaultSymbols
+using Simulation.Units
+using DataFrames
+
+kernels = dispersal_dists = fill(2.0km, 10 * 10)
+kernel = GaussianKernel.(dispersal_dists, 1e-10)
+
+@test_nowarn EpiMovement(kernel)
+move = EpiMovement(kernel)
+@test typeof(move.home) <: AlwaysMovement
+@test typeof(move.work) <: Commuting
+@test move.home.kernels == kernel
+
+work = DataFrame([[1], [1], [1.0]], [:from, :to, :count])
+@test_nowarn EpiMovement(kernel, work)
+move = EpiMovement(kernel, work)
+@test typeof(move.home) <: AlwaysMovement
+@test typeof(move.work) <: Commuting
+@test move.home.kernels == kernel
+@test move.work.home_to_work == work
