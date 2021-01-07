c0b5fcb928735da64cb02561d94d167c64e93e04
Claire Harris <claire.harris@bioss.ac.uk>
2021-01-07 10:35:47 +0000

    First pass at documentation.

---
+++ docs/make.jl
@@ -0,0 +1,21 @@
+using Documenter
+using Simulation
+
+makedocs(
+    modules = [Simulation],
+    sitename = "Simulation.jl",
+    format=Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true",
+    ),
+    pages = [
+        "Home" => "index.md",
+    ],
+    strict=true,
+    checkdocs=:none,
+)
+
+deploydocs(
+    repo = "github.com/boydorr/Simulation.jl.git",
+    # This is the branch that in-development docs are built for (default is master)
+    devbranch = "master",
+)
