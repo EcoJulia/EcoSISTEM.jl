333572842998b55ce8ef0f565e707ce917c29622
Claire Harris <claire.harris@bioss.ac.uk>
2021-03-26 11:54:10 +0000

    Update Africa_run for workstation - save output at intervals, otherwise too large.

--- examples/Biodiversity/Africa_run.jl
+++ examples/Biodiversity/Africa_run.jl
@@ -70,6 +70,15 @@ end
 gif(anim, "examples/Biodiversity/Africa.gif", fps = 30)
 
 
+using Simulation
+using Simulation.ClimatePref
+using Simulation.Units
+using Unitful
+using Unitful.DefaultSymbols
+using JLD
+file = "Documents/Chapter5/data/Africa.tif"
+africa = readfile(file, -25°, 50°, -35°, 40°)
+active =  Array{Bool, 2}(.!isnan.(africa'))
 # Set up initial parameters for ecosystem
 numSpecies = 50_000; grid = size(africa); req= 10.0kJ; individuals=3*10^8; area = 64e6km^2; totalK = 1000.0kJ/km^2
 
@@ -112,16 +121,27 @@ rel = Gauss{typeof(1.0K)}()
 
 #Create ecosystem
 eco = Ecosystem(sppl, abenv, rel)
-eco.abundances.grid[50_000, :] .= 0
+eco.abundances.matrix[50_000, :] .= 0
+
+function simulate!(eco::Ecosystem, times::Unitful.Time, timestep::Unitful.Time, cacheInterval::Unitful.Time, cacheFolder::String, scenario_name::String)
+  time_seq = 0s:timestep:times
+  counting = 0
+  for i in 1:length(time_seq)
+      update!(eco, timestep);
+      # Save cache of abundances
+      if mod(time_seq[i], cacheInterval) == 0year
+          JLD.save(joinpath(cacheFolder, scenario_name * (@sprintf "%02d.jld" uconvert(NoUnits,time_seq[i]/cacheInterval))), "abun", eco.abundances.matrix)
+      end
+  end
+end
 
 # Simulation Parameters
-burnin = 10years; times = 50years; timestep = 1month; record_interval = 1month; repeats = 1
+burnin = 10years; times = 50years; timestep = 1month; record_interval = 12months;
 lensim = length(0years:record_interval:times)
-abuns = zeros(Int64, numSpecies, prod(grid), lensim)
-@time simulate!(abuns, eco, burnin, timestep)
+@time simulate!(eco, burnin, timestep)
 rand_start = rand(findall(active), 1)[1]
 eco.abundances.grid[50_000, rand_start[1], rand_start[2]] = 100
-@time simulate_record!(abuns, eco, times, record_interval, timestep);
+@time simulate!(eco, times, timestep, record_interval, "sdc/Africa", "Africa_run");
 
 sumabun = sum(abuns[1:49_999, :, :, 1], dims = 1)
 abuns = reshape(abuns[1, :, :], grid[1], grid[2], lensim)
