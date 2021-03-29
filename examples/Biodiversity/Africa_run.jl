4d169aff4f16a72a18bd469e7ad270d5e509480f
Claire Harris <claire.harris@bioss.ac.uk>
2021-03-29 16:52:56 +0100

    New runs to compare specialist vs generalist and plot invasion speed.

--- examples/Biodiversity/Africa_run.jl
+++ examples/Biodiversity/Africa_run.jl
@@ -3,6 +3,8 @@ using Simulation.ClimatePref
 using Simulation.Units
 using Unitful
 using Unitful.DefaultSymbols
+using Distances
+using StatsBase
 using Plots
 file = "../Chapter5/data/Africa.tif"
 africa = readfile(file, -25°, 50°, -35°, 40°)
@@ -69,6 +71,92 @@ anim = @animate for i in 1:lensim
 end
 gif(anim, "examples/Biodiversity/Africa.gif", fps = 30)
 
+specialist_vars = [0.5K, 1.0K, 5.0K, 10.0K, 25.0K, 50.0K]
+velocity = zeros(typeof(1.0km/month), length(specialist_vars))
+rand_start = rand(findall(active), 1)[1]
+for i in eachindex(specialist_vars)
+    # Set up initial parameters for ecosystem
+    numSpecies = 2; grid = size(africa); req= 10.0kJ; individuals=0; area = 64e6km^2; totalK = 1000.0kJ/km^2
+
+    # Set up how much energy each species consumes
+    energy_vec = SolarRequirement(fill(req, numSpecies))
+
+
+    # Set rates for birth and death
+    birth = 0.6/year
+    death = 0.6/year
+    longevity = 1.0
+    survival = 0.1
+    boost = 1.0
+    # Collect model parameters together
+    param = EqualPop(birth, death, longevity, survival, boost)
+
+    # Create kernel for movement
+    kernel = fill(GaussianKernel(15.0km, 10e-10), numSpecies)
+    movement = AlwaysMovement(kernel, Torus())
+
+
+    # Create species list, including their temperature preferences, seed abundance and native status
+    opts = fill(274.0K, numSpecies)
+    vars = [50.0K, specialist_vars[i]]
+    traits = GaussTrait(opts, vars)
+    native = fill(true, numSpecies)
+    # abun = rand(Multinomial(individuals, numSpecies))
+    abun = fill(div(individuals, numSpecies), numSpecies)
+    sppl = SpeciesList(numSpecies, traits, abun, energy_vec,
+        movement, param, native)
+    sppl.params.birth
+
+    # Create abiotic environment - even grid of one temperature
+    abenv = simplehabitatAE(274.0K, grid, totalK, area, active)
+
+    # Set relationship between species and environment (gaussian)
+    rel = Gauss{typeof(1.0K)}()
+
+    #Create ecosystem
+    eco = Ecosystem(sppl, abenv, rel)
+    eco.abundances.grid[1, rand_start[1], rand_start[2]] = 100
+
+    # Simulation Parameters
+    burnin = 100years; times = 100years; timestep = 1month; record_interval = 1month; repeats = 1
+    lensim = length(0years:record_interval:times)
+    simulate!(eco, burnin,timestep)
+    eco.abundances.grid[2, rand_start[1], rand_start[2]] = 100
+    abuns = zeros(Int64, numSpecies, prod(grid), lensim)
+    @time simulate_record!(abuns, eco, times, record_interval, timestep);
+
+    abuns = reshape(abuns[:, :, :, 1], numSpecies, grid[1], grid[2], lensim)
+    origin = [rand_start[1], rand_start[2]]
+    dest = findall(abuns[2, :, :, 1] .> 0)
+    inst_velocity = map(1:lensim) do t
+        dest = findall(abuns[2, :, :, t] .> 0)
+        dists = [euclidean(origin, [dest[i][1], dest[i][2]]) for i in length(dest)] .* getgridsize(eco)
+        return maximum(dists)/month
+    end
+    velocity[i] = mean(inst_velocity)
+    # anim = @animate for i in 1:lensim
+    #     africa_abun1 = Float64.(abuns[1, :, :, i])
+    #     africa_abun1[.!(active)] .= NaN
+    #     africa_abun2 = Float64.(abuns[2, :, :, i])
+    #     africa_abun2[.!(active)] .= NaN
+    #     heatmap(africa_abun1, clim = (0, 700_000),
+    #     background_color = :lightblue,
+    #     background_color_outside=:white,
+    #     grid = false, color = cgrad(:algae, scale = :exp),
+    #     aspect_ratio = 1, layout = (@layout [a b]), subplot = 1)
+    #     heatmap!(africa_abun2, clim = (0, 700_000),
+    #     background_color = :lightblue,
+    #     background_color_outside=:white,
+    #     grid = false, color = cgrad(:algae, scale = :exp),
+    #     aspect_ratio = 1, subplot = 2)
+    # end
+    # gif(anim, "examples/Biodiversity/Africa_$i.gif", fps = 30)
+end
+
+plot(ustrip.(abs.(specialist_vars .- 50.0K)), ustrip.(velocity),
+    xlab = "Selective advantage", ylab = "Invasion speed (km/month)",
+    label = "")
+Plots.pdf("examples/Biodiversity/Invasion.pdf")
 
 using Simulation
 using Simulation.ClimatePref
