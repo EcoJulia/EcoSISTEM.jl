1471ef9617edb17592f219e3a8e7a1d1f3239529
Claire Harris <claire.harris@bioss.ac.uk>
2021-04-01 13:15:41 +0100

    Update examples for paper.

--- examples/Biodiversity/Africa_run.jl
+++ examples/Biodiversity/Africa_run.jl
@@ -1,3 +1,5 @@
+#### SINGLE SPECIES ####
+
 using Simulation
 using Simulation.ClimatePref
 using Simulation.Units
@@ -70,6 +72,8 @@ anim = @animate for i in 1:lensim
     heatmap(africa_abun, clim = (0, 700_000), background_color = :lightblue, background_color_outside=:white, grid = false, color = cgrad(:algae, scale = :exp), aspect_ratio = 1)
 end
 gif(anim, "examples/Biodiversity/Africa.gif", fps = 30)
+
+#### SPECIALIST VERSUS GENERALIST ####
 
 specialist_vars = [0.5K, 1.0K, 5.0K, 10.0K, 25.0K, 50.0K]
 velocity = zeros(typeof(1.0km/month), length(specialist_vars))
@@ -155,15 +159,18 @@ end
 
 plot(ustrip.(abs.(specialist_vars .- 50.0K)), ustrip.(velocity),
     xlab = "Selective advantage", ylab = "Invasion speed (km/month)",
-    label = "")
+    label = "", grid = false)
 Plots.pdf("examples/Biodiversity/Invasion.pdf")
 
+
+#### SPECIALIST VERSUS MANY GENERALISTS ####
 using Simulation
 using Simulation.ClimatePref
 using Simulation.Units
 using Unitful
 using Unitful.DefaultSymbols
 using JLD
+using Printf
 file = "Documents/Chapter5/data/Africa.tif"
 africa = readfile(file, -25°, 50°, -35°, 40°)
 active =  Array{Bool, 2}(.!isnan.(africa'))
@@ -178,7 +185,7 @@ energy_vec = SolarRequirement(fill(req, numSpecies))
 birth = 0.6/year
 death = 0.6/year
 longevity = 1.0
-survival = 0.0
+survival = 0.1
 boost = 1.0
 # Collect model parameters together
 param = EqualPop(birth, death, longevity, survival, boost)
@@ -211,6 +218,7 @@ rel = Gauss{typeof(1.0K)}()
 eco = Ecosystem(sppl, abenv, rel)
 eco.abundances.matrix[50_000, :] .= 0
 
+import Simulation.simulate!
 function simulate!(eco::Ecosystem, times::Unitful.Time, timestep::Unitful.Time, cacheInterval::Unitful.Time, cacheFolder::String, scenario_name::String)
   time_seq = 0s:timestep:times
   counting = 0
@@ -224,19 +232,137 @@ function simulate!(eco::Ecosystem, times::Unitful.Time, timestep::Unitful.Time, 
 end
 
 # Simulation Parameters
-burnin = 10years; times = 50years; timestep = 1month; record_interval = 12months;
+burnin = 100years; times = 100years; timestep = 1month; record_interval = 12months;
 lensim = length(0years:record_interval:times)
 @time simulate!(eco, burnin, timestep)
 rand_start = rand(findall(active), 1)[1]
 eco.abundances.grid[50_000, rand_start[1], rand_start[2]] = 100
-@time simulate!(eco, times, timestep, record_interval, "sdc/Africa", "Africa_run");
+@time simulate!(eco, times, timestep, record_interval, "sdc/Africa/specialist", "Africa_run");
 
-sumabun = sum(abuns[1:49_999, :, :, 1], dims = 1)
-abuns = reshape(abuns[1, :, :], grid[1], grid[2], lensim)
 
-anim = @animate for i in 1:lensim
-    africa_abun = Float64.(abuns[:, :, i])
-    africa_abun[.!(active)] .= NaN
-    heatmap(africa_abun, clim = (0, 700_000), background_color = :lightblue, background_color_outside=:white, grid = false, color = cgrad(:algae, scale = :exp), aspect_ratio = 1)
+#### 50,000 SPECIES COEXISTING #####
+
+using Simulation
+using Simulation.ClimatePref
+using Simulation.Units
+using Unitful
+using Unitful.DefaultSymbols
+using JLD
+using Printf
+
+file = "Documents/Chapter5/data/Africa.tif"
+africa = readfile(file, -25°, 50°, -35°, 40°)
+active =  Array{Bool, 2}(.!isnan.(africa'))
+# Set up initial parameters for ecosystem
+numSpecies = 50_000; grid = size(africa); req= 10.0kJ; individuals=3*10^8; area = 64e6km^2; totalK = 1000.0kJ/km^2
+
+# Set up how much energy each species consumes
+energy_vec = SolarRequirement(fill(req, numSpecies))
+
+
+# Set rates for birth and death
+birth = 0.6/year
+death = 0.6/year
+longevity = 1.0
+survival = 0.1
+boost = 1.0
+# Collect model parameters together
+param = EqualPop(birth, death, longevity, survival, boost)
+
+# Create kernel for movement
+kernel = fill(GaussianKernel(15.0km, 10e-10), numSpecies)
+movement = AlwaysMovement(kernel, Torus())
+
+
+# Create species list, including their temperature preferences, seed abundance and native status
+opts = fill(274.0K, numSpecies)
+vars = fill(50.0K, numSpecies)
+traits = GaussTrait(opts, vars)
+native = fill(true, numSpecies)
+# abun = rand(Multinomial(individuals, numSpecies))
+abun = fill(div(individuals, numSpecies), numSpecies)
+sppl = SpeciesList(numSpecies, traits, abun, energy_vec,
+    movement, param, native)
+sppl.params.birth
+
+# Create abiotic environment - even grid of one temperature
+abenv = simplehabitatAE(274.0K, grid, totalK, area, active)
+
+
+# Set relationship between species and environment (gaussian)
+rel = Gauss{typeof(1.0K)}()
+
+#Create ecosystem
+eco = Ecosystem(sppl, abenv, rel)
+
+import Simulation.simulate!
+function simulate!(eco::Ecosystem, times::Unitful.Time, timestep::Unitful.Time, cacheInterval::Unitful.Time, cacheFolder::String, scenario_name::String)
+  time_seq = 0s:timestep:times
+  counting = 0
+  for i in 1:length(time_seq)
+      update!(eco, timestep);
+      # Save cache of abundances
+      if mod(time_seq[i], cacheInterval) == 0year
+          JLD.save(joinpath(cacheFolder, scenario_name * (@sprintf "%02d.jld" uconvert(NoUnits,time_seq[i]/cacheInterval))), "abun", eco.abundances.matrix)
+      end
+  end
 end
-gif(anim, fps = 30)
+
+# Simulation Parameters
+burnin = 10years; times = 100years; timestep = 1month; record_interval = 12months;
+lensim = length(0years:record_interval:times)
+@time simulate!(eco, burnin, timestep)
+@time simulate!(eco, times, timestep, record_interval, "sdc/Africa", "Africa_run_coexist");
+
+using JLD
+using Plots
+using Diversity
+abuns = load("examples/Biodiversity/Africa_run_coexist100.jld", "abun")
+meta = Metacommunity(abuns)
+div = norm_sub_alpha(meta, 0)
+sumabuns = reshape(div[!, :diversity], 100, 100)
+heatmap(sumabuns,
+    background_color = :lightblue,
+    background_color_outside=:white,
+    grid = false, color = :algae,
+    aspect_ratio = 1, layout = (@layout [a b; c d]),
+    clim = (0, 50_000), margin = 0.5 * Plots.mm,
+    title = "A", titleloc = :left)
+
+abuns = load("examples/Biodiversity/Africa_run50.jld", "abun")
+meta = Metacommunity(abuns)
+div = norm_sub_alpha(meta, 0)
+sumabuns = reshape(div[!, :diversity], 100, 100)
+heatmap!(sumabuns,
+    background_color = :lightblue,
+    background_color_outside=:white,
+    grid = false, color = :algae,
+    aspect_ratio = 1, subplot = 2,
+    clim = (0, 50_000), right_margin = 2.0 * Plots.mm,
+    title = "B", titleloc = :left)
+
+abuns = load("examples/Biodiversity/Africa_run100.jld", "abun")
+meta = Metacommunity(abuns)
+div = norm_sub_alpha(meta, 0)
+sumabuns = reshape(div[!, :diversity], 100, 100)
+heatmap!(sumabuns,
+    background_color = :lightblue,
+    background_color_outside=:white,
+    grid = false, color = :algae,
+    aspect_ratio = 1, subplot = 3,
+    clim = (0, 50_000), right_margin = 2.0 * Plots.mm,
+    title = "C", titleloc = :left)
+
+
+abuns = load("examples/Biodiversity/Africa_run50.jld", "abun")
+meta = Metacommunity(abuns)
+div = norm_sub_rho(meta, 1.0)
+sumabuns = reshape(div[!, :diversity], 100, 100)
+heatmap!(sumabuns,
+    background_color = :lightblue,
+    background_color_outside=:white,
+    grid = false, color = :algae,
+    aspect_ratio = 1, subplot = 4,
+     right_margin = 2.0 * Plots.mm,
+    title = "D", titleloc = :left, clim = (0, 1))
+Plots.pdf("examples/Biodiversity/Africa.pdf")
