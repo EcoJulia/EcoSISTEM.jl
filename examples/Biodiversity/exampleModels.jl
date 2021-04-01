625f33fe67280320e5a3eb4fda45118ca502eb70
Claire Harris <claire.harris@bioss.ac.uk>
2021-04-01 16:46:10 +0100

    Updates to example models.

--- examples/Biodiversity/exampleModels.jl
+++ examples/Biodiversity/exampleModels.jl
@@ -240,7 +240,7 @@ param = EqualPop(birth, death, l, s , boost)
 
 # Create ecosystem
 
-movement = BirthOnlyMovement(kernel, Torus())
+movement = BirthOnlyMovement(kernel, NoBoundary())
 
 traits = GaussTrait(opts, vars)
 native = fill(true, numSpecies)
@@ -301,7 +301,7 @@ for i in eachindex(grids)
 
     # Create ecosystem
 
-    movement = BirthOnlyMovement(kernel, Torus())
+    movement = BirthOnlyMovement(kernel, NoBoundary())
 
     traits = GaussTrait(opts, vars)
     native = fill(true, numSpecies)
@@ -357,7 +357,7 @@ for i in eachindex(areas)
 
     # Create ecosystem
 
-    movement = BirthOnlyMovement(kernel, Torus())
+    movement = BirthOnlyMovement(kernel, NoBoundary())
 
     traits = GaussTrait(opts, vars)
     native = fill(true, numSpecies)
@@ -379,13 +379,13 @@ left_margin = 20.0 *Plots.mm)
 ## Sustain large number of species ##
 times = 10years; timestep = 1month
 lensim = length(0month:timestep:times)
-reps = 100
-areas = [10.0,20.0,50.0,100.0]
-SR = zeros(Float64, length(areas), reps)
-for r in reps
-    for i in eachindex(areas)
-        numSpecies = 100; grd = (10,10); req=(450000.0kJ/m^2, 192.0nm/m^2);
-        individuals = 100_000_000; area = areas[i].*km^2;
+reps = 10
+species = [100, 500, 1000, 5_000]
+SR = zeros(Float64, length(species), reps)
+for r in 1:reps
+    for i in eachindex(species)
+        numSpecies = species[i]; grd = (10,10); req=(450000.0kJ/m^2, 192.0nm/m^2);
+        individuals = 100_000_000; area = 100.0km^2;
         totalK = (4.5e11kJ/km^2, 192.0mm/km^2)
 
         abenv1 = simplehabitatAE(298.0K, grd, totalK[1], area)
@@ -393,28 +393,28 @@ for r in reps
         bud = BudgetCollection2(abenv1.budget, abenv2.budget)
         abenv = GridAbioticEnv{typeof(abenv1.habitat), typeof(bud)}(abenv1.habitat, abenv1.active, bud, abenv1.names)
 
-        vars = rand(Uniform(2.0, 5.0), numSpecies) .* K
-        opts = rand(Uniform(278.0, 308.0), numSpecies) .* K
+        vars = rand(Uniform(1.0, 5.0), numSpecies) .* K
+        opts = 298.0K .+ vars .* range(-3, stop = 3, length = numSpecies)
         av_dist = rand(Uniform(0.6, 2.4), numSpecies) .* km
         kernel = GaussianKernel.(av_dist, 10e-10)
 
-        death = 0.15/ year
+        death = abs.(rand(Normal(0.15, 0.135), numSpecies)) ./year
         birth = death
         l = 1.0
         s = 0.1
         boost = 1.0
 
-        size_mean = 1.0m^2
+        size_mean = rand(Normal(1.0, 0.5), numSpecies) .* m^2
         # Set up how much energy each species consumes
-        energy_vec1 = SolarRequirement(fill(req[1] * size_mean, numSpecies))
-        energy_vec2 = WaterRequirement(fill(req[2] * size_mean, numSpecies))
+        energy_vec1 = SolarRequirement(abs.(req[1] .* size_mean))
+        energy_vec2 = WaterRequirement(abs.(req[2] .* size_mean))
 
         energy_vec = ReqCollection2(energy_vec1, energy_vec2)
-        param = EqualPop(birth, death, l, s , boost)
+        param = PopGrowth{typeof(unit(birth[1]))}(birth, death, l, s , boost)
 
         # Create ecosystem
 
-        movement = BirthOnlyMovement(kernel, Torus())
+        movement = BirthOnlyMovement(kernel, NoBoundary())
 
         traits = GaussTrait(opts, vars)
         native = fill(true, numSpecies)
@@ -424,62 +424,85 @@ for r in reps
         rel = Gauss{typeof(first(opts))}()
         eco = Ecosystem(sppl, abenv, rel)
         simulate!(eco, times, timestep)
-        SR[i, r] = meta_gamma(eco, 0.0)[1, :diversity]
+        SR[i, r] = sum(sum(eco.abundances.matrix, dims = 2) .> 0)
+        print(".")
     end
 end
 
-bar!(string.(areas), SR, grid = false, xlab = "Area (km²)",
-ylab = "Total abundance", guidefontsize = 16,
+meanSR = dropdims(mean(SR, dims = 2), dims = 2)
+sdSR = dropdims(std(SR, dims = 2), dims = 2)
+
+bar(string.(species), meanSR ./species, yerr= sdSR, grid = false, xlab = "Number of species introduced",
+ylab = "% Species survived", guidefontsize = 16,
 tickfontsize= 16, titlefontsize=24, margin = 10.0*Plots.mm,
-label = "", subplot = 4, title = "D", titleloc = :left,
-left_margin = 20.0 *Plots.mm)
+label = "",  title = "D", titleloc = :left,
+left_margin = 20.0 *Plots.mm, ylim = (0, 1))
 
 ## DISPERSAL ##
+times = 50years; timestep = 1month
+lensim = length(0month:timestep:times)
 distances = [0.5, 1.0, 2.0, 4.0]
+endabuns = zeros(Int64, 10, 10, length(distances))
 for i in eachindex(distances)
     numSpecies = 2; grd = (10,10); req=(450000.0kJ/m^2, 192.0nm/m^2);
-    individuals = 100_000_000; area = 100.0*km^2;
+    individuals = 0; area = 100.0km^2;
     totalK = (4.5e11kJ/km^2, 192.0mm/km^2)
 
-    scenario = [SimpleScenario(TempIncrease!, 0.0K/10year)]
-
-    divfuns = [sorenson, meta_speciesrichness, meta_shannon, meta_simpson, mean_abun, geom_mean_abun, norm_meta_alpha, raw_meta_alpha, norm_meta_beta, raw_meta_beta, norm_meta_rho, raw_meta_rho, meta_gamma]
-    q = 1.0
-    a = Int64(i)
-    scenario_names = ["Neutral$a"]
-
-    simDict = Dict("times" => 10years, "burnin" => 0year, "interval" => 1month, "timestep" => 1month, "scenarios" => scenario, "divfuns" => divfuns, "q" => q, "reps" => 1, "scenario_names" => scenario_names, "cacheInterval" => 1years)
-    lensim = length(0month:simDict["interval"]:simDict["times"])
-
-    abenv1 = simplehabitatAE(299.0K, grd, totalK[1], area)
-    abenv2 = simplehabitatAE(299.0K, grd, totalK[2], area)
+    abenv1 = simplehabitatAE(298.0K, grd, totalK[1], area)
+    abenv2 = simplehabitatAE(298.0K, grd, totalK[2], area)
     bud = BudgetCollection2(abenv1.budget, abenv2.budget)
     abenv = GridAbioticEnv{typeof(abenv1.habitat), typeof(bud)}(abenv1.habitat, abenv1.active, bud, abenv1.names)
 
     vars = fill(2.0, numSpecies) .* K
     opts = fill(298.0, numSpecies) .* K
-
     av_dist = fill(distances[i], numSpecies) .* km
-    kernel = GaussianKernel(av_dist, 10e-10)
+    kernel = GaussianKernel.(av_dist, 10e-10)
 
-    death_rates = abs.(rand(Normal(0.15, 0.135), numSpecies)) ./year
-    birth_rates = death_rates
+    death = 0.15/ year
+    birth = death
+    l = 1.0
+    s = 0.1
+    boost = 1.0
 
-    paramDict = Dict("numSpecies" => numSpecies, "numInvasive" => 0, "numIndiv" => individuals, "reqs" => req, "opts" => opts, "vars" => vars, "birth" => birth_rates, "death" => death_rates, "s" => 5e-2, "boost" => 1.0, "kernel" => kernel, "totalK" => totalK, "bound" => NoBoundary())
+    size_mean = 1.0m^2
+    # Set up how much energy each species consumes
+    energy_vec1 = SolarRequirement(fill(req[1] * size_mean, numSpecies))
+    energy_vec2 = WaterRequirement(fill(req[2] * size_mean, numSpecies))
 
-    diver = zeros(length(divfuns), lensim, length(scenario))
-    Chapter3.newrun!(diver, abenv, paramDict, simDict, 1, "data/Examples/disperse")
+    energy_vec = ReqCollection2(energy_vec1, energy_vec2)
+    param = EqualPop(birth, death, l, s , boost)
+
+    # Create ecosystem
+
+    movement = BirthOnlyMovement(kernel, NoBoundary())
+
+    traits = GaussTrait(opts, vars)
+    native = fill(true, numSpecies)
+    abun = rand(Multinomial(individuals, numSpecies))
+    sppl = SpeciesList(numSpecies, traits, abun, energy_vec,
+        movement, param, native)
+    rel = Gauss{typeof(first(opts))}()
+    eco = Ecosystem(sppl, abenv, rel)
+    eco.abundances.grid[1, :, 1] .= 100.0
+    eco.abundances.grid[2, :, 10] .= 100.0
+    simulate!(eco, times, timestep)
+    endabuns[:, :, i] = sum(eco.abundances.matrix, dims = 1)
 end
 
-endabun = mapslices(sum, JLD.load("data/Examples/disperse/cache/Neutral110001.jld", "abun"), dims = 1)[1,:]
-endabun = reshape(endabun, 10, 10)
-
-heatmap(grid = false, xlab = "Distance (km)", ylab = "Distance (km)", size = (1200, 800), guidefontsize = 12,tickfontsize= 12, titlefontsize=18, margin = 2.0*Plots.mm, legendfontsize = 12, label = "", layout = (@layout [a b; c d]), clim = (0, 1e6), link = :both)
+heatmap(grid = false, xlab = "Distance (km)",
+ylab = "Distance (km)", size = (1200, 800),
+guidefontsize = 12,tickfontsize= 12, titlefontsize=18,
+margin = 10.0*Plots.mm, legendfontsize = 12, label = "",
+layout = (@layout [a b; c d]), link = :both)
+titles = ["A", "B", "C", "D"]
 for i in 1:4
-    endabun = mapslices(sum, JLD.load("data/Examples/disperse/cache/Neutral$i"*"10001.jld", "abun"), dims = 1)[1,:]
-    endabun = reshape(endabun, 10, 10)
     m = distances[i]
-    display(heatmap!(1:10,1:10, endabun, grid = false, xlab = "Distance (km)", ylab = "Distance (km)", size = (1200, 800), guidefontsize = 12,tickfontsize= 12, titlefontsize=18, margin = 2.0*Plots.mm, legendfontsize = 12, label = "", layout = (@layout [a b; c d]), subplot = i, clim = (0, 1e6), link = :both, title ="Average dispersal of $m km"))
+    display(heatmap!(1:10,1:10, endabuns[:, :, i],
+    grid = false, xlab = "Distance (km)", ylab = "Distance (km)",
+    guidefontsize = 16, tickfontsize= 16,
+    titlefontsize=24, title = titles[i], margin = 10.0*Plots.mm,
+    label = "", subplot = i, titleloc = :left,
+    clim = (0, 1.5e4), link = :both))
 end
 
-Plots.pdf("plots/DispersalSD.pdf")
+Plots.pdf("examples/Biodiversity/DispersalSD.pdf")
