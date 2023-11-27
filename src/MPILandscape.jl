5de045c5a16246877c66a1f8e2c2afc38640e1dd
richardreeve <richard.reeve@glasgow.ac.uk>
2023-11-27 01:04:00 +0000

    Switch to package extensions

--- src/MPILandscape.jl
+++ src/MPILandscape.jl
@@ -1,51 +1,41 @@
-using ..MPI
+using EcoSISTEM
+using MPI
+using Random
 
-"""
-    MPIGridLandscape
+import EcoSISTEM: MPIGridLandscape, emptyMPIgridlandscape, synchronise_from_rows!, synchronise_from_cols!
 
-MPIEcosystem abundances housed in the landscape, shared across multiple nodes.
+function MPIGridLandscape(sppcounts::Vector{Int32}, sccounts::Vector{Int32},
+                          rows_matrix::Matrix{Int64}, cols_vector::Vector{Int64})
+                          rank = MPI.Comm_rank(MPI.COMM_WORLD)
 
-"""
-mutable struct MPIGridLandscape{RA <: Base.ReshapedArray, NT <: NamedTuple}
-  rows_matrix::Matrix{Int64}
-  cols_vector::Vector{Int64}
-  reshaped_cols::Vector{RA}
-  rows_tuple::NT
-  cols_tuple::NT
-  rngs::Vector{MersenneTwister}
+  totalspp = sum(sppcounts)
+  totalsc = sum(sccounts)
 
-  function MPIGridLandscape(sppcounts::Vector{Int32}, sccounts::Vector{Int32}, rows_matrix::Matrix{Int64}, cols_vector::Vector{Int64})
-    rank = MPI.Comm_rank(MPI.COMM_WORLD)
+  lastsp = sum(sppcounts[1:(rank + 1)])
+  firstsp = lastsp - sppcounts[rank + 1] + 1
 
-    totalspp = sum(sppcounts)
-    totalsc = sum(sccounts)
+  lastsc = sum(sccounts[1:(rank + 1)])
+  firstsc = lastsc - sccounts[rank + 1] + 1
 
-    lastsp = sum(sppcounts[1:(rank + 1)])
-    firstsp = lastsp - sppcounts[rank + 1] + 1
+  sppindices = [0; cumsum(sppcounts) .* sccounts[rank + 1]]
+  scindices = [0; cumsum(sccounts) .* sppcounts[rank + 1]]
 
-    lastsc = sum(sccounts[1:(rank + 1)])
-    firstsc = lastsc - sccounts[rank + 1] + 1
-
-    sppindices = [0; cumsum(sppcounts) .* sccounts[rank + 1]]
-    scindices = [0; cumsum(sccounts) .* sppcounts[rank + 1]]
-
-    reshaped_cols = map(1:length(sccounts)) do i
-      reshape(view(cols_vector, (sppindices[i] + 1) : sppindices[i + 1]),
-              Int64(sppcounts[i]), Int64(sccounts[rank + 1]))
-    end
-    rows = (total = totalspp, first = firstsp, last = lastsp,
-            counts = sccounts .* sppcounts[rank + 1])
-    cols = (total = totalsc, first = firstsc, last = lastsc,
-            counts = sppcounts .* sccounts[rank + 1])
-    (rows.last - rows.first + 1) * cols.total == length(rows_matrix) ||
-      error("rows_matrix size mismatch: $(rows.last - rows.first + 1) * $(cols.total) !=$(length(rows_matrix))")
-    (cols.last - cols.first + 1) * rows.total == length(cols_vector) ||
-      error("cols_vector size mismatch: $(cols.last - cols.first + 1) * $(rows.total) !=$(length(cols_vector))")
-
-    return new{typeof(reshaped_cols[1]), typeof(rows)}(rows_matrix,
-      cols_vector, reshaped_cols, rows, cols,
-      [MersenneTwister(rand(UInt)) for _ in 1:Threads.nthreads()])
+  reshaped_cols = map(1:length(sccounts)) do i
+    reshape(view(cols_vector, (sppindices[i] + 1) : sppindices[i + 1]),
+            Int64(sppcounts[i]), Int64(sccounts[rank + 1]))
   end
+  rows = (total = totalspp, first = firstsp, last = lastsp,
+          counts = sccounts .* sppcounts[rank + 1])
+  cols = (total = totalsc, first = firstsc, last = lastsc,
+          counts = sppcounts .* sccounts[rank + 1])
+  (rows.last - rows.first + 1) * cols.total == length(rows_matrix) ||
+    error("rows_matrix size mismatch: $(rows.last - rows.first + 1) * $(cols.total) !=$(length(rows_matrix))")
+  (cols.last - cols.first + 1) * rows.total == length(cols_vector) ||
+    error("cols_vector size mismatch: $(cols.last - cols.first + 1) * $(rows.total) !=$(length(cols_vector))")
+
+  return MPIGridLandscape{typeof(reshaped_cols[1]), typeof(rows)}(rows_matrix,
+                                                                  cols_vector, reshaped_cols, rows, cols,
+                                                                  [MersenneTwister(rand(UInt)) for _ in Base.OneTo(Threads.nthreads())])
 end
 
 """
