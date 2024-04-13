e0d6842b7de0d9e8f5c67b0754d127f7ac8294ed
richardreeve <richard.reeve@glasgow.ac.uk>
2024-04-14 00:25:23 +0100

    Fix plotting and testing for monthly climate data

--- ext/EcoSISTEMPlotsExt.jl
+++ ext/EcoSISTEMPlotsExt.jl
@@ -3,61 +3,53 @@ module EcoSISTEMPlotsExt
 using Unitful, Unitful.DefaultSymbols
 using EcoSISTEM, EcoSISTEM.ClimatePref, EcoSISTEM.Units
 using RecipesBase
+using AxisArrays
 # import Plots: px
 
 @info "Creating Plots recipes for EcoSISTEM..."
 
-# Recipe for plotting ERA data from a particular time period.
-@recipe function f(era::EcoSISTEM.ERA, time::Unitful.Time)
+const MONTHS = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+]
+
+# Recipe for plotting ERA and CERA data from a particular time period.
+@recipe function f(era::Union{ERA, CERA}, time::Unitful.Time)
     tm = ustrip.(uconvert(year, time))
     yr = floor(Int64, tm)
     ind = round(Int64, (tm - yr) / (1 / 12))
     typeof(ind) <: Int64 || error("NO")
-    mnth = [
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec"
-    ][ind + 1]
+    mnth = MONTHS[ind + 1]
     A = transpose(ustrip.(era.array[:, :, time]))
-    x = ustrip.(era.array.axes[1].val)
-    y = ustrip.(era.array.axes[2].val)
+    x = ustrip.(AxisArrays.axes(era.array, 1).val)
+    y = ustrip.(AxisArrays.axes(era.array, 2).val)
     seriestype := :heatmap
     grid --> false
     title --> "$yr $mnth"
     return x, y, A
 end
 
-@recipe function f(era::EcoSISTEM.ERA, time::Unitful.Time, xrange, yrange)
+@recipe function f(era::Union{ERA, CERA}, time::Unitful.Time, xrange, yrange)
     tm = ustrip.(uconvert(year, time))
     yr = floor(Int64, tm)
     ind = round(Int64, (tm - yr) / (1 / 12))
     typeof(ind) <: Int64 || error("NO")
-    mnth = [
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec"
-    ][ind + 1]
+    mnth = MONTHS[ind + 1]
     A = transpose(ustrip.(era.array[xrange, yrange, time]))
-    step1 = ustrip(era.array.axes[1].val[2] - era.array.axes[1].val[1])
-    step2 = ustrip(era.array.axes[2].val[2] - era.array.axes[2].val[1])
+    step1 = ustrip(AxisArrays.axes(era.array, 1).val[2] -
+                   AxisArrays.axes(era.array, 1).val[1])
+    step2 = ustrip(AxisArrays.axes(era.array, 2).val[2] -
+                   AxisArrays.axes(era.array, 2).val[1])
     x = ustrip(xrange.left):step1:ustrip(xrange.right)
     y = ustrip(yrange.left):step2:ustrip(yrange.right)
     seriestype := :heatmap
@@ -66,83 +58,12 @@ end
     return x, y, A
 end
 
-@recipe function f(cera::EcoSISTEM.CERA, time::Unitful.Time)
-    tm = ustrip.(uconvert(year, time))
-    yr = floor(Int64, tm)
-    ind = round(Int64, (tm - yr) / (1 / 12))
-    typeof(ind) <: Int64 || error("NO")
-    mnth = [
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec"
-    ][ind + 1]
-    A = transpose(ustrip.(cera.array[:, :, time + 1month]))
-    x = ustrip.(cera.array.axes[1].val)
-    y = ustrip.(cera.array.axes[2].val)
-    seriestype := :heatmap
-    grid --> false
-    title --> "$yr $mnth"
-    return x, y, A
-end
-
-@recipe function f(era::EcoSISTEM.CERA, time::Unitful.Time, xmin::typeof(1.0°),
-                   xmax::typeof(1.0°), ymin::typeof(1.0°), ymax::typeof(1.0°))
-    tm = ustrip.(uconvert(year, time))
-    yr = floor(Int64, tm)
-    ind = round(Int64, (tm - yr) / (1 / 12))
-    typeof(ind) <: Int64 || error("NO")
-    mnth = [
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec"
-    ][ind + 1]
-    A = transpose(ustrip.(era.array[xmin .. xmax, ymin .. ymax, time + 1month]))
-    step1 = ustrip(era.array.axes[1].val[2] - era.array.axes[1].val[1])
-    step2 = ustrip(era.array.axes[2].val[2] - era.array.axes[2].val[1])
-    x = (ustrip.(xmin) + step1):step1:(ustrip.(xmax))
-    y = (ustrip.(ymin) + step1):step1:(ustrip.(ymax))
-    seriestype := :heatmap
-    grid --> false
-    title --> "$yr $mnth"
-    return x, y, A
-end
-
-@recipe function f(wc::Worldclim_monthly, time::Unitful.Time)
+@recipe function f(wc::Union{Worldclim_monthly, CHELSA_monthly},
+                   time::Unitful.Time)
     ind = (time + 1month) / month
     typeof(ind) <: Int64 || error("NO")
-    mnth = [
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec"
-    ][ind]
-    A = transpose(ustrip.(wc.array[:, :, time + 1month]))
+    mnth = MONTHS[ind]
+    A = transpose(ustrip.(wc.array[:, :, time]))
     x = ustrip.(AxisArrays.axes(wc.array, 1).val)
     y = ustrip.(AxisArrays.axes(wc.array, 2).val)
     seriestype := :heatmap
@@ -152,30 +73,18 @@ end
     return x, y, A
 end
 
-@recipe function f(wc::Worldclim_monthly, time::Unitful.Time,
-                   xmin::typeof(1.0°),
-                   xmax::typeof(1.0°), ymin::typeof(1.0°), ymax::typeof(1.0°))
+@recipe function f(wc::Union{Worldclim_monthly, CHELSA_monthly},
+                   time::Unitful.Time, xrange, yrange)
     ind = (time + 1month) / month
     typeof(ind) <: Int64 || error("NO")
-    mnth = [
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec"
-    ][ind]
-    A = transpose(ustrip.(wc.array[xmin .. xmax, ymin .. ymax, time + 1month]))
-    step1 = ustrip(wc.array.axes[1].val[2] - wc.array.axes[1].val[1])
-    step2 = ustrip(wc.array.axes[2].val[2] - wc.array.axes[2].val[1])
-    x = (ustrip.(xmin) + step1):step1:(ustrip.(xmax))
-    y = (ustrip.(ymin) + step1):step1:(ustrip.(ymax))
+    mnth = MONTHS[ind]
+    A = transpose(ustrip.(wc.array[xrange, yrange, time]))
+    step1 = ustrip(AxisArrays.axes(wc.array, 1).val[2] -
+                   AxisArrays.axes(wc.array, 1).val[1])
+    step2 = ustrip(AxisArrays.axes(wc.array, 2).val[2] -
+                   AxisArrays.axes(wc.array, 2).val[1])
+    x = ustrip(xrange.left):step1:ustrip(xrange.right)
+    y = ustrip(yrange.left):step2:ustrip(yrange.right)
     seriestype := :heatmap
     grid --> false
     background_color_inside --> :grey
@@ -183,39 +92,43 @@ end
     return x, y, A
 end
 
+#=
 """
     getprofile(spp_names::Vector{String}, data::IndexedTable, variable_name::String, dims::Tuple{Int64, Int64} = (1,1))
 
 Function to plot climate profiles for a vector of species names, `spp_names`, using a JuliaDB table of GBIF records, `data`, column containing climate variable of interest, `var`, and dimensions over which it should be plotted, `dims`.
 """
-# function getprofile(spp_names::Vector{String}, data::IndexedTable, var::Symbol, label::String, dims::Tuple{Int64, Int64} = (1,1))
-
-#     # Check for dimensions to be greater or the same as the length of species, or for all to be plotted in one plot pane.
-#     (dims[1] * dims[2] >= length(spp_names) || dims == (1,1)) || error("Dimensions not big enough for number of species")
-#     first_plot = true
-#     # Loop through species names, adding profiles to plot
-#     hist = histogram(layout = dims)
-#     for i in eachindex(spp_names)
-#         # Filter data for species in question
-#         spp = filter(p-> p[:species] == spp_names[i], data)
-#         # Select climate variable of interest
-#         vals = select(spp, var)
-#         # Remove NaNs
-#         res = vcat(vals...)
-#         res = res[.!isnan.(res)]
-
-#         # If dimensions are (1,1)...
-#         # ... all plotted to the same subplot window, else individual for each species
-#         sp = ifelse(dims == (1,1), 1, i)
-#         # ... put legend on the left, otherwise none
-#         lg = ifelse(dims == (1,1), :left, :none)
-#         # ... title as species name, else blank
-#         title = ifelse(dims == (1,1), "", spp_names[i])
-#         # For the first species plot histogram, otherwise add to previous plot
-#         histogram!(res, bins = -20:2:30, grid = false, xlabel = label, label=spp_names[i], subplot = sp, legend= lg, top_margin = 20px,
-#         bottom_margin = 20px, title = title)
-#     end
-#     return hist
-# end
+function getprofile(spp_names::Vector{String}, data::IndexedTable, var::Symbol,
+                    label::String, dims::Tuple{Int64, Int64} = (1, 1))
+    # Check for dimensions to be greater or the same as the length of species, or for all to be plotted in one plot pane.
+    (dims[1] * dims[2] >= length(spp_names) || dims == (1, 1)) ||
+        error("Dimensions not big enough for number of species")
+    first_plot = true
+    # Loop through species names, adding profiles to plot
+    hist = histogram(layout = dims)
+    for i in eachindex(spp_names)
+        # Filter data for species in question
+        spp = filter(p -> p[:species] == spp_names[i], data)
+        # Select climate variable of interest
+        vals = select(spp, var)
+        # Remove NaNs
+        res = vcat(vals...)
+        res = res[.!isnan.(res)]
+        # If dimensions are (1,1)...
+        # ... all plotted to the same subplot window, else individual for each species
+        sp = ifelse(dims == (1, 1), 1, i)
+        # ... put legend on the left, otherwise none
+        lg = ifelse(dims == (1, 1), :left, :none)
+        # ... title as species name, else blank
+        title = ifelse(dims == (1, 1), "", spp_names[i])
+        # For the first species plot histogram, otherwise add to previous plot
+        histogram!(res, bins = -20:2:30, grid = false, xlabel = label,
+                   label = spp_names[i], subplot = sp, legend = lg,
+                   top_margin = 20px,
+                   bottom_margin = 20px, title = title)
+    end
+    return hist
+end
+=#
 
 end
--- test/ext_EcoSISTEMPlotsExt.jl
+++ test/ext_EcoSISTEMPlotsExt.jl
@@ -12,8 +12,22 @@ using Test
                      Axis{:longitude}((0°):(1°):(10°)),
                      Axis{:time}((2000year):(1year):(2020year)))
     era = EcoSISTEM.ERA(temp)
-    @test plot(era, 2001year).n == 1
+    @test plot(era, 2000year).n == 1
     @test plot(era, 2002year, 1° .. 4°, 5° .. 10°).n == 1
+    cera = EcoSISTEM.CERA(temp)
+    @test plot(cera, 2001year).n == 1
+    @test plot(cera, 2020year, 1° .. 4°, 5° .. 10°).n == 1
+
+    temp = AxisArray(fill(2.0K, 11, 11, 12),
+                     Axis{:latitude}((0°):(1°):(10°)),
+                     Axis{:longitude}((0°):(1°):(10°)),
+                     Axis{:time}(January:(1month):December))
+    wm = EcoSISTEM.Worldclim_monthly(temp)
+    @test plot(wm, January).n == 1
+    @test plot(wm, September, 0° .. 2°, 5° .. 10°).n == 1
+    chelsa = EcoSISTEM.CHELSA_monthly(temp)
+    @test plot(chelsa, February).n == 1
+    @test plot(chelsa, December, 3° .. 10°, 0° .. 3°).n == 1
 end
 
 end
