8192e771049f3015402477f8a99f1585f8a3c95a
richardreeve <richard.reeve@glasgow.ac.uk>
2024-04-16 21:58:14 +0100

    Let it run for 12 hours

--- examples/HPC/MARS-demo.bash
+++ examples/HPC/MARS-demo.bash
@@ -6,7 +6,7 @@
 #SBATCH --output=%x-%j.out        # output file name will contain job name + job ID
 #SBATCH --error=%x-%j.err         # error file name will contain job name + job ID
 #SBATCH --partition=smp           # which partition to use, default on MARS is “nodes" (or "smp", "gpu", "gpuplus")
-#SBATCH --time=0-01:00:00         # time limit for the whole run, in the form of d-hh:mm:ss, also accepts mm, mm:ss, hh:mm:ss, d-hh, d-hh:mm
+#SBATCH --time=0-12:00:00         # time limit for the whole run, in the form of d-hh:mm:ss, also accepts mm, mm:ss, hh:mm:ss, d-hh, d-hh:mm
 #SBATCH --mem=512GB               # memory required per node, in the form of [num][M|G|T]
 #SBATCH --nodes=1                 # number of nodes to allocate, default is 1
 #SBATCH --ntasks=1                # number of Slurm tasks to be launched, increase for multi-process runs ex. MPI
