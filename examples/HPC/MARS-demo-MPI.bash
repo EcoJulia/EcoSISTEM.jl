896c6dd031256fcf4c87f3ab3f5e5312f56f788f
richardreeve <richard.reeve@glasgow.ac.uk>
2024-04-18 02:15:40 +0100

    Try srun for sbatch call

--- examples/HPC/MARS-demo-MPI.bash
+++ examples/HPC/MARS-demo-MPI.bash
@@ -28,4 +28,4 @@ export OMP_NUM_THREADS=1
 export JULIA_NUM_THREADS=8
 
 ############# MY CODE #############
-mpirun -N 8 -n 32 julia --project=examples examples/HPC/MPIRun.jl
+srun -N 8 -n 32 julia --project=examples examples/HPC/MPIRun.jl
