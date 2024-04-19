d3736d4df67e93d2dfb069fb06fe82eeef5e587c
nhz2 <nhz2@cornell.edu>
2024-04-18 20:56:50 -0400

    use p7zip_jll

--- ext/EcoSISTEMDataPipelineExt.jl
+++ ext/EcoSISTEMDataPipelineExt.jl
@@ -1,24 +1,13 @@
 module EcoSISTEMDataPipelineExt
 
 using EcoSISTEM
-using ZipArchives
+import p7zip_jll
 
 @info "Creating data pipeline interface for EcoSISTEM..."
 
 function EcoSISTEM.unziptemp(path::String)
-    zr = ZipReader(read(open(path)))
     newpath = mktempdir()
-    files = zip_names(zr)
-    for file in files
-        if zip_isdir(zr, file)
-            mkdir(joinpath(newpath, file))
-        end
-    end
-    for file in files
-        if !zip_isdir(zr, file)
-            write(joinpath(newpath, file), zip_readentry(zr, file, String))
-        end
-    end
+    run(`$(p7zip_jll.p7zip()) x -tzip -y -o$(newpath) $(path)`)
     @debug "Unzipped to $newpath"
     return newpath
 end
