6ef7ef0107c731d159f39beb0e2ff07a502d214a
richardreeve <richard.reeve@glasgow.ac.uk>
2024-04-25 11:06:17 +0100

    Rename HPC scripts

--- examples/HPC/MARS-demo-MPI-threads.bash
+++ examples/HPC/MARS-demo-MPI-threads.bash
@@ -2,16 +2,15 @@
 
 ############# SLURM SETTINGS #############
 #SBATCH --account=project0000     # account name (mandatory), if the job runs under a project then it'll be the project name, if not then it should =none
-#SBATCH --job-name=MPIRun-1x8x8   # some descriptive job name of your choice
 #SBATCH --output=%x-%j.out        # output file name will contain job name + job ID
 #SBATCH --error=%x-%j.err         # error file name will contain job name + job ID
 #SBATCH --partition=nodes         # which partition to use, default on MARS is “nodes" (or "smp", "gpu", "gpuplus")
 #SBATCH --time=0-12:00:00         # time limit for the whole run, in the form of d-hh:mm:ss, also accepts mm, mm:ss, hh:mm:ss, d-hh, d-hh:mm
 #SBATCH --mem=256G                # memory required per node, in the form of [num][M|G|T]
 #SBATCH --nodes=1                 # number of nodes to allocate, default is 1
-#SBATCH --ntasks=8                # number of Slurm tasks to be launched, increase for multi-process runs ex. MPI
-#SBATCH --cpus-per-task=8         # number of processor cores to be assigned for each task, default is 1, increase for multi-threaded runs
-#SBATCH --ntasks-per-node=8       # number of tasks to be launched on each allocated node
+#SBATCH --ntasks=1                # number of Slurm tasks to be launched, increase for multi-process runs ex. MPI
+#SBATCH --cpus-per-task=64         # number of processor cores to be assigned for each task, default is 1, increase for multi-threaded runs
+#SBATCH --ntasks-per-node=1       # number of tasks to be launched on each allocated node
 #SBATCH --threads-per-core=1      # Threads per core
 
 ############# LOADING MODULES (optional) #############
@@ -24,7 +23,7 @@ julia --project=examples -e 'using Pkg; Pkg.instantiate(); Pkg.build("MPI"); usi
 # any threaded system libraries from automatically
 # using threading. Then manually set Julia threads
 export OMP_NUM_THREADS=1
-export JULIA_NUM_THREADS=8
+export JULIA_NUM_THREADS=64
 
 ############# MY CODE #############
-bin/mpiexecjl --project=examples -n 8 julia -t 8 --project=examples examples/HPC/MPIRun.jl
+bin/mpiexecjl --project=examples -n 1 julia -t 64 --project=examples examples/HPC/MPIRun.jl
--- examples/HPC/MARS-demo-threads.bash
+++ examples/HPC/MARS-demo-threads.bash
@@ -2,29 +2,26 @@
 
 ############# SLURM SETTINGS #############
 #SBATCH --account=project0000     # account name (mandatory), if the job runs under a project then it'll be the project name, if not then it should =none
-#SBATCH --job-name=MPIRun-1x64x1  # some descriptive job name of your choice
 #SBATCH --output=%x-%j.out        # output file name will contain job name + job ID
 #SBATCH --error=%x-%j.err         # error file name will contain job name + job ID
-#SBATCH --partition=nodes         # which partition to use, default on MARS is “nodes" (or "smp", "gpu", "gpuplus")
+#SBATCH --partition=smp           # which partition to use, default on MARS is “nodes" (or "smp", "gpu", "gpuplus")
 #SBATCH --time=0-12:00:00         # time limit for the whole run, in the form of d-hh:mm:ss, also accepts mm, mm:ss, hh:mm:ss, d-hh, d-hh:mm
-#SBATCH --mem=256G                # memory required per node, in the form of [num][M|G|T]
+#SBATCH --mem=512GB               # memory required per node, in the form of [num][M|G|T]
 #SBATCH --nodes=1                 # number of nodes to allocate, default is 1
-#SBATCH --ntasks=64               # number of Slurm tasks to be launched, increase for multi-process runs ex. MPI
-#SBATCH --cpus-per-task=1         # number of processor cores to be assigned for each task, default is 1, increase for multi-threaded runs
-#SBATCH --ntasks-per-node=64      # number of tasks to be launched on each allocated node
+#SBATCH --ntasks=1                # number of Slurm tasks to be launched, increase for multi-process runs ex. MPI
+#SBATCH --cpus-per-task=128       # number of processor cores to be assigned for each task, default is 1, increase for multi-threaded runs
+#SBATCH --ntasks-per-node=1       # number of tasks to be launched on each allocated node
 #SBATCH --threads-per-core=1      # Threads per core
 
 ############# LOADING MODULES (optional) #############
 module load apps/julia
-module load mpi/openmpi
-julia --project=examples -e 'using Pkg; Pkg.instantiate(); Pkg.build("MPI"); using MPI; MPI.install_mpiexecjl(destdir = "bin", force = true)'
 
 ############# ENVIRONMENT #############
 # Set the number of OpenMP threads to 1 to prevent
 # any threaded system libraries from automatically
 # using threading. Then manually set Julia threads
 export OMP_NUM_THREADS=1
-export JULIA_NUM_THREADS=1
+export JULIA_NUM_THREADS=128
 
 ############# MY CODE #############
-bin/mpiexecjl --project=examples -n 64 julia -t 1 --project=examples examples/HPC/MPIRun.jl
+julia -t 128 --project=examples examples/Africa_run.jl
