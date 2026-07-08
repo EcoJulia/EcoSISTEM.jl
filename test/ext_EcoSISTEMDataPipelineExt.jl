5dd366ac0cf482c9cf77956e54c3f0e73464c474
Richard Reeve <git@richardreeve.net>
2026-07-08 01:55:25 +0100

    - Reconcile compats between module and examples folder - Remove DataPipeline dependency temporarily - Minor whitepace fixes

--- test/ext_EcoSISTEMDataPipelineExt.jl
+++ test/ext_EcoSISTEMDataPipelineExt.jl
@@ -3,13 +3,12 @@
 module ExtEcoSISTEMDataPipelineExt
 
 using EcoSISTEM
-using DataPipeline
 using Test
 
 if !Sys.iswindows()
     if isdir("assets")
         @testset "DataPipeline unziptemp() test" begin
-            @test isdir(EcoSISTEM.unziptemp("assets/WorldClim/BioClim/zips/wc2.1_10m_bio.zip"))
+            # @test isdir(EcoSISTEM.unziptemp("assets/WorldClim/BioClim/zips/wc2.1_10m_bio.zip"))
         end
     end
 end
